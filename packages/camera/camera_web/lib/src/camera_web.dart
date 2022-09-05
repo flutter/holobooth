4a673aa3425125c315b9ac078514a726090979d3
alestiago <dev@alestiago.com>
2022-09-05 17:22:54 +0100

    refactor: pefer final for variable declarations if they are not reassigned

--- packages/camera/camera_web/lib/src/camera_web.dart
+++ packages/camera/camera_web/lib/src/camera_web.dart
@@ -81,7 +81,7 @@ class CameraPlugin extends CameraPlatform {
       final devices =
           await html.window.navigator.mediaDevices?.enumerateDevices() ?? [];
       for (var deviceIndex = 0; deviceIndex < devices.length; deviceIndex++) {
-        dynamic device = devices[deviceIndex];
+        final dynamic device = devices[deviceIndex];
         if (device is html.MediaDeviceInfo && device.kind == 'videoinput') {
           videoDevices.add(
               MediaDeviceInfo(deviceId: device.deviceId, label: device.label));
