ead1121041674aa0e447f7663b87de844c3ca7aa
Oscar <martinm.oscar@gmail.com>
2022-09-06 19:13:11 +0200

    refactor: cast nullable to non nullable

--- test/app/widgets/footer_link_test.dart
+++ test/app/widgets/footer_link_test.dart
@@ -16,7 +16,7 @@ class _MockUrlLauncher extends Mock
 
 bool findTextAndTap(InlineSpan visitor, String text) {
   if (visitor is TextSpan && visitor.text == text) {
-    (visitor.recognizer as TapGestureRecognizer).onTap?.call();
+    (visitor.recognizer as TapGestureRecognizer?)?.onTap?.call();
 
     return false;
   }
