dd5819baa6f4334c15f4a12fd766c7a281de39a1
Oscar <martinm.oscar@gmail.com>
2022-09-12 11:57:02 +0200

    chore: update libraries

--- packages/tensorflow_models/tensorflow_models_platform_interface/lib/tensorflow_models_platform_interface.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/lib/tensorflow_models_platform_interface.dart
@@ -5,5 +5,7 @@ import 'dart:typed_data';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:tensorflow_models_platform_interface/src/method_channel/method_channel_tensorflow_models.dart';
 
+import 'src/method_channel/method_channel_tensorflow_models.dart';
+
 part 'src/platform_interface/tensorflow_models_platform.dart';
 part 'src/types/posenet.dart';
