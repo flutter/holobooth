d2d0497bd4176569c67878c475402694e312627e
alestiago <dev@alestiago.com>
2022-09-13 10:18:03 +0100

    test: resolved characters_layers_test.dart errors

--- test/photobooth/widgets/characters_layer_test.dart
+++ test/photobooth/widgets/characters_layer_test.dart
@@ -11,16 +11,16 @@ import '../../helpers/helpers.dart';
 class _MockPhotoboothBloc extends MockBloc<PhotoboothEvent, PhotoboothState>
     implements PhotoboothBloc {}
 
-void main() {
-  const width = 1;
-  const height = 1;
-  const data = '';
-  const image = CameraImage(width: width, height: height, data: data);
+class _MockPhotoboothCameraImage extends Mock implements PhotoboothCameraImage {
+}
 
+void main() {
+  late PhotoboothCameraImage image;
   late PhotoboothBloc photoboothBloc;
 
   group('CharactersLayer', () {
     setUp(() {
+      image = _MockPhotoboothCameraImage();
       photoboothBloc = _MockPhotoboothBloc();
     });
 
