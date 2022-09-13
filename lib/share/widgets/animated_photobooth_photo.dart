3c03da85dcd86888fcd9e5076fd62c78646c4f99
alestiago <dev@alestiago.com>
2022-09-13 10:07:38 +0100

    refactor: removed unused imports

--- lib/share/widgets/animated_photobooth_photo.dart
+++ lib/share/widgets/animated_photobooth_photo.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_photobooth/photobooth/photobooth.dart';
--- test/photobooth/view/photobooth_page_test.dart
+++ test/photobooth/view/photobooth_page_test.dart
@@ -1,12 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:convert';
-
 import 'package:bloc_test/bloc_test.dart';
-import 'package:camera/camera.dart';
 import 'package:camera_platform_interface/camera_platform_interface.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_photobooth/assets.g.dart';
