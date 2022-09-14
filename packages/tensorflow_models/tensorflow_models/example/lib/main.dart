4359d04e6ae60b7cb01da351b9bd46817829412f
Oscar <martinm.oscar@gmail.com>
2022-09-14 11:45:25 +0200

    feat: example of tensorflow with static image (#65)
    * chore: split examples
    
    * chore: example with image
    
    * chore: analyzer
    
    * chore: moving out image loader package
    
    * chore: fix feedback
    
    * chore: adding launch
    
    * chore: example
    
    * chore: feedback

--- packages/tensorflow_models/tensorflow_models/example/lib/main.dart
+++ packages/tensorflow_models/tensorflow_models/example/lib/main.dart
@@ -1,114 +1,39 @@
 // ignore_for_file: avoid_print
 
-import 'dart:typed_data';
-
+import 'package:example/sample_single_image.dart';
 import 'package:flutter/material.dart';
-import 'package:tensorflow_models/tensorflow_models.dart';
-
-const List<int> kTransparentImage = <int>[
-  0x89,
-  0x50,
-  0x4E,
-  0x47,
-  0x0D,
-  0x0A,
-  0x1A,
-  0x0A,
-  0x00,
-  0x00,
-  0x00,
-  0x0D,
-  0x49,
-  0x48,
-  0x44,
-  0x52,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x08,
-  0x06,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x15,
-  0xC4,
-  0x89,
-  0x00,
-  0x00,
-  0x00,
-  0x0A,
-  0x49,
-  0x44,
-  0x41,
-  0x54,
-  0x78,
-  0x9C,
-  0x63,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x05,
-  0x00,
-  0x01,
-  0x0D,
-  0x0A,
-  0x2D,
-  0xB4,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x49,
-  0x45,
-  0x4E,
-  0x44,
-  0xAE,
-];
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: App());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  PoseNet? _net;
-
-  @override
-  void initState() {
-    super.initState();
-    _initializePosenet();
-  }
-
-  Future<void> _initializePosenet() async {
-    _net?.dispose();
-    _net = await load();
-    final pose = await _net!.estimateSinglePose(
-      ImageData(
-        data: Uint8ClampedList.fromList(kTransparentImage),
-        width: 4,
-        height: 4,
-      ),
-    );
-    print(pose.score);
-    for (final keypoint in pose.keypoints) {
-      print(keypoint.part);
-      print(keypoint.score);
-    }
-  }
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tensorflow Examples')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).push(SingleCapturePage.route());
+              },
+              child: const Text('Posenet - Single image'),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
