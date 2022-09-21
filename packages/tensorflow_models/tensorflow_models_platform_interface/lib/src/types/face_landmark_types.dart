dfd66197de381f206a39e6aa85f876ee783c361d
Alejandro Santiago <dev@alestiago.com>
2022-09-21 14:13:41 +0100

    refactor: improved Face serialisation and documentation (#76)
    * docs: improved face documentation
    
    * refactor: made face constructor private
    
    * docss: improved Face documentation
    
    * refactor: cleaned Face deserialization

--- packages/tensorflow_models/tensorflow_models_platform_interface/lib/src/types/face_landmark_types.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/lib/src/types/face_landmark_types.dart
@@ -1,36 +1,62 @@
+import 'dart:collection';
+
 typedef Faces = List<Face>;
 
+abstract class FaceLandmarksDetector {
+  Future<Faces> estimateFaces(
+    dynamic object, {
+    EstimationConfig estimationConfig = const EstimationConfig(),
+  });
+
+  void dispose();
+}
+
+/// {@template types.face_landmar_types.Face}
+/// A face detected by [FaceLandmarksDetector].
+///
+/// The face is represented by [keypoints].
+/// {@endtemplate}
+class Face {
+  const Face._(this.keypoints);
+
+  factory Face.fromJson(Map<String, dynamic> json) {
+    final keypoints = json['keypoints'] as List<dynamic>;
+    return Face._(
+      UnmodifiableListView(
+        keypoints.map(
+          (keypoint) => Keypoint.fromJson(keypoint as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
+
+  /// Points representing the face landmarks.
+  ///
+  /// The order of the [keypoints] is significant, the mappings of these indexes
+  /// to face locations can be found at:
+  /// * https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/mesh_map.jpg
+  final UnmodifiableListView<Keypoint> keypoints;
+}
+
+/// Representation of a [Face] landmark point.
 class Keypoint {
-  Keypoint(this.x, this.y, this.z, this.score, this.name);
+  Keypoint._(this.x, this.y, this.z, this.score, this.name);
+
+  factory Keypoint.fromJson(Map<String, dynamic> json) {
+    return Keypoint._(
+      json['x'] as num,
+      json['y'] as num,
+      json['z'] as num?,
+      json['score'] as num?,
+      json['name'] as String?,
+    );
+  }
 
   final num x;
   final num y;
   final num? z;
   final num? score;
   final String? name;
-}
-
-class Face {
-  const Face(this.keypoints);
-
-  factory Face.fromJs(List<dynamic> keyPointsJs) {
-    return Face(
-      keyPointsJs.map(
-        (e) {
-          final map = e as Map<String, dynamic>;
-          return Keypoint(
-            map['x'] as num,
-            map['y'] as num,
-            map['z'] as num?,
-            map['score'] as num?,
-            map['name'] as String?,
-          );
-        },
-      ).toList(),
-    );
-  }
-
-  final List<Keypoint> keypoints;
 }
 
 /// {@template types.face_landmark_types.EstimationConfig}
@@ -59,13 +85,4 @@ class EstimationConfig {
   ///
   /// * https://google.github.io/mediapipe/solutions/face_mesh.html#static_image_mode
   final bool staticImageMode;
-}
-
-abstract class FaceLandmarksDetector {
-  Future<Faces> estimateFaces(
-    dynamic object, {
-    EstimationConfig estimationConfig = const EstimationConfig(),
-  });
-
-  void dispose();
 }
