5e34ec03047bba0596f2408a7b4db8ed4b17f3f7
alestiago <dev@alestiago.com>
2022-09-22 10:20:31 +0100

    refactor: sorted imports

--- packages/tensorflow_models/tensorflow_models/example/lib/src/pages/landmarks_open_mouth_page.dart
+++ packages/tensorflow_models/tensorflow_models/example/lib/src/pages/landmarks_open_mouth_page.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
-import 'dart:math' as math;
-
 // TODO(alestiago): Use a plugin instead.
 // ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
+import 'dart:math' as math;
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
 import 'package:tensorflow_models/tensorflow_models.dart' as tf;
 
 class LandmarksOpenMouthPage extends StatelessWidget {
