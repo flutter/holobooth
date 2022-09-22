8c089c3b36a25a08f66bbc32c23538bed46b177b
Oscar <martinm.oscar@gmail.com>
2022-09-22 13:23:42 +0200

    chore: docs

--- packages/tensorflow_models/tensorflow_models/lib/src/tensor_flow_face_landmarks.dart
+++ packages/tensorflow_models/tensorflow_models/lib/src/tensor_flow_face_landmarks.dart
@@ -1,6 +1,10 @@
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
 
 // TODO(oscar): Check if we should use singleton class or load model everytime.
+// TODO(oscar): document this class
+//Brief sentence explaining what it is
+//A url to the official tensorflow face landmark documentation
+//A url to the source code from tsjs-models
 abstract class TensorFlowFaceLandmarks {
   static Future<FaceLandmarksDetector> load() async =>
       TensorflowModelsPlatform.instance.loadFaceLandmark();
