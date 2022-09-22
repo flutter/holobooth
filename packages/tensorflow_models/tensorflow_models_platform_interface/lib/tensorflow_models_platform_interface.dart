51d589d75e089b8cd8d400bb3d5373b88d9e9b24
Oscar <martinm.oscar@gmail.com>
2022-09-22 11:14:45 +0200

    chore: remove posenet (#79)

--- packages/tensorflow_models/tensorflow_models_platform_interface/lib/tensorflow_models_platform_interface.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/lib/tensorflow_models_platform_interface.dart
@@ -1,4 +1,5 @@
 library ranodm_library_mine;
 
+export 'src/method_channel/method_channel_tensorflow_models.dart';
 export 'src/platform_interface/tensorflow_models_platform.dart';
 export 'src/types/types.dart';
