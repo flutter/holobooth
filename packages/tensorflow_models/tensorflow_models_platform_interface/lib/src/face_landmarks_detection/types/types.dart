c060b66ea6337bdfff49116909950c7f87d0f627
Oscar <martinm.oscar@gmail.com>
2022-09-22 14:43:14 +0200

    chore: fix

--- packages/tensorflow_models/tensorflow_models_platform_interface/lib/src/face_landmarks_detection/types/types.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/lib/src/face_landmarks_detection/types/types.dart
@@ -5,11 +5,6 @@
 /// * TypeScript types at: https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/src/types.ts
 import 'dart:collection';
 
-/// Equivalent Dart types matching TensorFlow's TypeScript implementation.
-///
-/// See also:
-/// * TypeScript types at: https://github.com/tensorflow/tfjs-models/blob/master/face-landmarks-detection/src/types.ts
-
 typedef Faces = List<Face>;
 
 abstract class FaceLandmarksDetector {
