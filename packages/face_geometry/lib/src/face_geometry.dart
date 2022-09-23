1e4d850da3b9a722b4fae8e0813ccba482f186a8
alestiago <dev@alestiago.com>
2022-09-23 17:32:51 +0100

    feat: moved extensions to faceGeometry

--- packages/face_geometry/lib/src/face_geometry.dart
+++ packages/face_geometry/lib/src/face_geometry.dart
@@ -5,22 +5,25 @@ import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_
 
 /// Set of calculations to detect common face expressions.
 extension FaceGeometry on tf.Face {
-  /// The distance between the top lip and the bottom lip in proportion to the
-  /// face height.
-  ///
-  /// The value is always number between 0 and 1, representing the percentage of
-  /// the mouth height in relation to the face height.
+  /// The distance between the top lip and the bottom lip.
   double get mouthDistance {
     final topLipPoint = keypoints[13];
     final bottomLipPoint = keypoints[14];
-    final verticalDistance = topLipPoint.distanceTo(bottomLipPoint);
+    return topLipPoint.distanceTo(bottomLipPoint);
+  }
 
-    final topForehead = keypoints[10];
-    final bottomChin = keypoints[152];
-    // TODO(alestiago): Get the height from bounding box.
-    final faceHeight = topForehead.distanceTo(bottomChin);
+  /// The distance between the top left eye lid and the bottom left eye lid.
+  double get leftEyeDistance {
+    final topEyeLid = keypoints[159];
+    final bottomEyeLid = keypoints[145];
+    return topEyeLid.distanceTo(bottomEyeLid);
+  }
 
-    return verticalDistance / faceHeight;
+  /// The distance between the top right eye lid and the bottom right eye lid
+  double get rightEyeDistance {
+    final topEyeLid = keypoints[386];
+    final bottomEyeLid = keypoints[374];
+    return topEyeLid.distanceTo(bottomEyeLid);
   }
 }
 
@@ -28,7 +31,6 @@ extension on tf.Keypoint {
   double distanceTo(tf.Keypoint other) {
     final dx = x - other.x;
     final dy = y - other.y;
-    final dz = (z ?? 0) - (other.z ?? 0);
-    return math.sqrt(math.pow(dx, 2) + math.pow(dy, 2) + math.pow(dz, 2));
+    return math.sqrt(math.pow(dx, 2) + math.pow(dy, 2));
   }
 }
