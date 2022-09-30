85c6111fafb5bd28164065d9c8e65faae89c33d8
alestiago <dev@alestiago.com>
2022-09-30 12:03:21 +0100

    chore: very_good create -t flutter_pkg photobooth_camera

---
+++ packages/photobooth_camera/test/src/photobooth_camera_test.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:photobooth_camera/photobooth_camera.dart';
+
+void main() {
+  group('PhotoboothCamera', () {
+    test('can be instantiated', () {
+      expect(PhotoboothCamera(), isNotNull);
+    });
+  });
+}
