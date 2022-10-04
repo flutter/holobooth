62a31f42c6ae1c5af20d76d4e0d35dfc3eddba53
Oscar <martinm.oscar@gmail.com>
2022-10-04 19:19:20 +0200

    feat: background to multi capture

---
+++ lib/multiple_capture/widgets/camera_background.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:io_photobooth/photobooth/photobooth.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+
+class CameraBackground extends StatelessWidget {
+  const CameraBackground({
+    super.key,
+    required this.aspectRatio,
+    required this.child,
+  });
+
+  final double aspectRatio;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const PhotoboothBackground(),
+        Center(
+          child: AspectRatio(
+            aspectRatio: aspectRatio,
+            child: ColoredBox(
+              color: PhotoboothColors.black,
+              child: child,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
