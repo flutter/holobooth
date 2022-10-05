411d2d2d66a1e85f65022e0ed00e04fb6b462afd
Laura177 <laura.c.buffington@gmail.com>
2022-10-05 14:51:29 -0700

    test tensorflow_models_platform

---
+++ packages/tensorflow_models/tensorflow_models_platform_interface/test/src/platform_interface/tensorflow_models_platform_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
+
+class MockTFModelsPlatform extends Mock implements TensorflowModelsPlatform {}
+
+void main() {
+  late TensorflowModelsPlatform tensorflowModelsPlatform;
+  setUp(() {
+    tensorflowModelsPlatform = MockTFModelsPlatform();
+  });
+
+  group('TensorflowModelsPlatform', () {
+    test('can be instantiated', () {
+      expect(tensorflowModelsPlatform, isNotNull);
+    });
+    group('loadFaceLandmark', () {
+      test('FaceLandmarkDetector', () {
+        when(() => tensorflowModelsPlatform.loadFaceLandmark())
+            .thenThrow(UnimplementedError());
+      });
+    });
+  });
+}
