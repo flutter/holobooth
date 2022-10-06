ea1303d4ac81a2b8c3e509b8905db4fe2603823b
Laura177 <laura.c.buffington@gmail.com>
2022-10-05 22:19:03 -0700

    initiate tests and create ci

---
+++ packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tensorflow_models/tensorflow_models.dart';
+
+class MockTensorFlowFaceLandmarks extends Mock
+    implements TensorFlowFaceLandmarks {}
+
+void main() {
+  late TensorFlowFaceLandmarks _tensorflowFaceLandmarks;
+
+  setUp(() {
+    _tensorflowFaceLandmarks = MockTensorFlowFaceLandmarks();
+  });
+  group('TensorFlowFaceLandmarks', () {
+    test('can be instantiated', () {
+      expect(_tensorflowFaceLandmarks, isNotNull);
+    });
+  });
+}
