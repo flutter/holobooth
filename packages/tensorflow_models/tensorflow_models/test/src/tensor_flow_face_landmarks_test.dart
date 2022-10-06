fcc99478c4a5e4eeb0c6966ce2ed2cbb59e1441a
Laura177 <laura.c.buffington@gmail.com>
2022-10-06 09:31:44 -0700

    modify load method test

--- packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
+++ packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
@@ -7,23 +7,21 @@ import 'package:tensorflow_models/tensorflow_models.dart';
 class MockTensorFlowFaceLandmarks extends Mock
     implements TensorFlowFaceLandmarks {}
 
-class MockTFModelsPlatform extends Mock implements TensorflowModelsPlatform {}
-
 void main() {
   late TensorFlowFaceLandmarks _tensorflowFaceLandmarks;
-  late TensorflowModelsPlatform _modelsPlatform;
 
   setUp(() {
     _tensorflowFaceLandmarks = MockTensorFlowFaceLandmarks();
-    _modelsPlatform = MockTFModelsPlatform();
   });
   group('TensorFlowFaceLandmarks', () {
     test('can be instantiated', () {
       expect(_tensorflowFaceLandmarks, isNotNull);
     });
-    test('loads unimplemented detector', () {
-      when(() => _modelsPlatform.loadFaceLandmark())
-          .thenThrow(UnimplementedError());
+    test('loads unimplemented faceLandmark', () {
+      expect(
+        () => TensorflowModelsPlatform.instance.loadFaceLandmark(),
+        throwsA(isA<UnimplementedError>()),
+      );
     });
   });
 }
