50f0da68a5856020e91672e679fc63d1d31f98d0
Laura177 <laura.c.buffington@gmail.com>
2022-10-06 12:33:04 -0700

    test: mock imageData

--- packages/tensorflow_models/tensorflow_models_platform_interface/test/src/types/image_data_test.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/test/src/types/image_data_test.dart
@@ -1,10 +1,20 @@
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
 
+class MockImageData extends Mock implements ImageData {}
+
 void main() {
+  late ImageData _imageData;
+
+  setUp(() {
+    _imageData = MockImageData();
+  });
   group('Image Data', () {
     test('can be instaniated', () {
-      expect(ImageData, isNotNull);
+      expect(_imageData, isNotNull);
     });
   });
 }
