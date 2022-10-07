b72180a6b1c45c9f2d85b24cd725b9ba5a4ad6dc
Oscar <martinm.oscar@gmail.com>
2022-10-07 15:10:03 +0200

    chore: workflow for image compositor

--- packages/image_compositor/test/io_image_compositor_test.dart
+++ packages/image_compositor/test/io_image_compositor_test.dart
@@ -1,7 +1,7 @@
 @TestOn('!chrome')
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:image_compositor/image_compositor.dart';
+import 'package:test/test.dart';
 
 void main() {
   group('ImageCompositor', () {
