338d92ea5aeecff50b4549bc68c50c7c9e0a1c9b
alestiago <dev@alestiago.com>
2022-10-10 09:16:13 +0100

    chore: removed extra files

--- packages/gif_compositor/gif_compositor_platform_interface/lib/gif_compositor_platform_interface.dart
+++ packages/gif_compositor/gif_compositor_platform_interface/lib/gif_compositor_platform_interface.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:gif_compositor_platform_interface/src/method_channel_gif_compositor.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
@@ -14,7 +7,8 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 /// rather than implement it as `GifCompositor`.
 /// Extending this class (using `extends`) ensures that the subclass will get
 /// the default implementation, while platform implementations that `implements`
-///  this interface will be broken by newly added [GifCompositorPlatform] methods.
+/// this interface will be broken by newly added [GifCompositorPlatform]
+/// methods.
 abstract class GifCompositorPlatform extends PlatformInterface {
   /// Constructs a GifCompositorPlatform.
   GifCompositorPlatform() : super(token: _token);
