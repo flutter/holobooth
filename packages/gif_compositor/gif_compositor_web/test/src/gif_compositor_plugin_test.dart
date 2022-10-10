246ee4f2cea0bf4614b75b8ea81a94131ac954fa
alestiago <dev@alestiago.com>
2022-10-10 14:19:56 +0100

    test: included tests

---
+++ packages/gif_compositor/gif_compositor_web/test/src/gif_compositor_plugin_test.dart
@@ -0,0 +1,42 @@
+/// **NOTE**: This test file should be runned on web, to do so use:
+/// ``` flutter test --platform chrome ```
+@TestOn('chrome')
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gif_compositor_platform_interface/gif_compositor_platform_interface.dart';
+import 'package:gif_compositor_web/src/gif_compositor_plugin.dart';
+import 'package:isolated_worker/js_isolated_worker.dart' as isolate;
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('$GifCompositorWeb', () {
+    test('can be registered', () {
+      GifCompositorWeb.registerWith();
+      expect(GifCompositorPlatform.instance, isA<GifCompositorWeb>());
+    });
+
+    group('composite', () {
+      test(
+        'throws an AssertionError when fileName does not end in ".gif"',
+        () {
+          expect(
+            () => GifCompositorWeb().composite(
+              images: [],
+              fileName: 'test',
+            ),
+            throwsAssertionError,
+          );
+        },
+      );
+
+      test(
+        'returns normally',
+        () {
+          // TODO(alestiago): Investigate on how to mock the singleton
+          // `JsIsolatedWorkerImpl`.
+        },
+      );
+    });
+  });
+}
