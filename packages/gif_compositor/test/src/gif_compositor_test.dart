014d5fb141fc0e956033a6a6f9ca1d2e8cf9a44b
alestiago <dev@alestiago.com>
2022-10-10 09:02:55 +0100

    chore: very_good create -t dart_pkg gif_compositor

---
+++ packages/gif_compositor/test/src/gif_compositor_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:gif_compositor/gif_compositor.dart';
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+
+void main() {
+  group('GifCompositor', () {
+    test('can be instantiated', () {
+      expect(GifCompositor(), isNotNull);
+    });
+  });
+}
