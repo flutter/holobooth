666ba130c421f6d2daf77000de01317eb28094b7
Laura177 <laura.c.buffington@gmail.com>
2022-10-12 12:51:54 -0700

    chore: switch to Fake implementation for test

--- packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
+++ packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
@@ -1,23 +1,17 @@
-// ignore_for_file: no_leading_underscores_for_local_identifiers
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:tensorflow_models/tensorflow_models.dart';
 
-class MockTensorFlowFaceLandmarks extends Mock
+class FakeTensorFlowFaceLandmarks extends Fake
     implements TensorFlowFaceLandmarks {}
 
 void main() {
-  late TensorFlowFaceLandmarks _tensorflowFaceLandmarks;
-
-  setUp(() {
-    _tensorflowFaceLandmarks = MockTensorFlowFaceLandmarks();
-  });
   group('TensorFlowFaceLandmarks', () {
     test('can be instantiated', () {
-      expect(_tensorflowFaceLandmarks, isNotNull);
+      final fakeFaceLandmarks = FakeTensorFlowFaceLandmarks();
+      expect(fakeFaceLandmarks, isNotNull);
     });
-    test('loads unimplemented faceLandmark', () {
+
+    test('throws an UnimplementedError', () {
       expect(
         TensorFlowFaceLandmarks.load,
         throwsA(isA<UnimplementedError>()),
