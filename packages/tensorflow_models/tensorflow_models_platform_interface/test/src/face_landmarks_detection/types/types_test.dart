d4c269493cc9d4a73cb1fda5529ed1e06918f853
Laura177 <laura.c.buffington@gmail.com>
2022-10-17 14:34:48 -0700

    test: add test for deserialized raw output

---
+++ packages/tensorflow_models/tensorflow_models_platform_interface/test/src/face_landmarks_detection/types/types_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
+
+void main() {
+  test('can be deserialized from raw output', () {
+    final jsonFaces = json.decode(estimateFacesOutput) as List;
+    for (final jsonFace in jsonFaces) {
+      expect(
+        () => Face.fromJson(jsonFace as Map<String, dynamic>),
+        returnsNormally,
+      );
+    }
+  });
+}
