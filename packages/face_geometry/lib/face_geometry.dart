772e5c78c07f7d16acc8cb62164c1fbd0937a732
alestiago <dev@alestiago.com>
2022-10-17 10:54:02 +0100

    feat: tested face_direction

--- packages/face_geometry/lib/face_geometry.dart
+++ packages/face_geometry/lib/face_geometry.dart
@@ -2,3 +2,4 @@ library face_geometry;
 
 export 'src/face_direction.dart';
 export 'src/face_geometry.dart';
+export 'src/vector3.dart';
--- packages/face_geometry/lib/src/face_direction.dart
+++ packages/face_geometry/lib/src/face_direction.dart
@@ -1,4 +1,4 @@
-import 'dart:math' as math;
+import 'package:face_geometry/face_geometry.dart';
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart'
     as tf;
 
@@ -27,35 +27,6 @@ extension FaceDirection on tf.Face {
       nose.z?.toDouble() ?? 0,
     );
     return _equationOfAPlane(leftCheeckVector, rightCheeckVector, noseVector);
-  }
-}
-
-/// {@template vector3}
-/// A 3D vector.
-/// {@endtemplate}
-class Vector3 {
-  /// {@macro vector3}
-  Vector3(this.x, this.y, this.z);
-
-  /// The x coordinate.
-  final double x;
-
-  /// The y coordinate.
-  final double y;
-
-  /// The z coordinate.
-  final double z;
-
-  Vector3 unit() {
-    final length = math.sqrt(x * x + y * y + z * z);
-    return Vector3(x / length, y / length, z / length);
-  }
-
-  Vector3 operator *(double value) => Vector3(x * value, y * value, z * value);
-
-  @override
-  String toString() {
-    return 'Vector3{x: $x, y: $y, z: $z}';
   }
 }
 
