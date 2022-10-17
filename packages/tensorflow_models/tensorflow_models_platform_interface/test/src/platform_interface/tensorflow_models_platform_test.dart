29f0c6e5297ae5a12a7008e689c7daa168b97fc3
alestiago <dev@alestiago.com>
2022-10-17 10:18:57 +0100

    refactor: cleaned tensorflow_models_platform_test.dart

--- packages/tensorflow_models/tensorflow_models_platform_interface/test/src/platform_interface/tensorflow_models_platform_test.dart
+++ packages/tensorflow_models/tensorflow_models_platform_interface/test/src/platform_interface/tensorflow_models_platform_test.dart
@@ -1,30 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
 
-class TFModelsPlatformTest extends TensorflowModelsPlatform {}
-
-class TFModelsPlatformBad extends Fake implements TensorflowModelsPlatform {}
+class _TestTensorflowModelsPlatform extends TensorflowModelsPlatform {}
 
 void main() {
   group('TensorflowModelsPlatform', () {
     group('instance', () {
       test('sets the instance', () {
-        final newPlatform = TFModelsPlatformTest();
-        expect(TensorflowModelsPlatform.instance, isNot(equals(newPlatform)));
+        final newPlatform = _TestTensorflowModelsPlatform();
+        expect(
+          TensorflowModelsPlatform.instance,
+          isNot(equals(newPlatform)),
+        );
         TensorflowModelsPlatform.instance = newPlatform;
         expect(TensorflowModelsPlatform.instance, newPlatform);
       });
-      test('throws assertion error if token is bad', () {
-        final badPlatform = TFModelsPlatformBad();
-        expect(
-          () => TensorflowModelsPlatform.instance = badPlatform,
-          throwsAssertionError,
-        );
-      });
     });
+
     group('loadFaceLandmark', () {
       test('throws UnimplementedError', () {
-        final platform = TFModelsPlatformTest();
+        final platform = _TestTensorflowModelsPlatform();
         expect(platform.loadFaceLandmark, throwsA(isA<UnimplementedError>()));
       });
     });
