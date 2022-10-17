9dede1d24798c95ab76aedabffbb6c843876dfe1
alestiago <dev@alestiago.com>
2022-10-17 09:41:10 +0100

    feat: subclassed over Fake

--- packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
+++ packages/tensorflow_models/tensorflow_models/test/src/tensor_flow_face_landmarks_test.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tensorflow_models/tensorflow_models.dart';
 
-class FakeTensorFlowFaceLandmarks extends Fake
-    implements TensorFlowFaceLandmarks {}
+class _TestTensorFlowFaceLandmarks extends TensorFlowFaceLandmarks {}
 
 void main() {
   group('TensorFlowFaceLandmarks', () {
     test('can be instantiated', () {
-      final fakeFaceLandmarks = FakeTensorFlowFaceLandmarks();
+      final fakeFaceLandmarks = _TestTensorFlowFaceLandmarks();
       expect(fakeFaceLandmarks, isNotNull);
     });
 
