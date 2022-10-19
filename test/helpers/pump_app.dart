0ce68f4beea9dfa23bcff00d851d1d9ef9f46756
Oscar <martinm.oscar@gmail.com>
2022-10-19 15:42:50 +0200

    chore: test

--- test/helpers/pump_app.dart
+++ test/helpers/pump_app.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_detector_repository/avatar_detector_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -7,27 +8,42 @@ import 'package:mocktail/mocktail.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
 import 'package:photos_repository/photos_repository.dart';
 
-class MockPhotosRepository extends Mock implements PhotosRepository {}
+class _MockPhotosRepository extends Mock implements PhotosRepository {}
+
+class _MockAvatarDetectorRepository extends Mock
+    implements AvatarDetectorRepository {
+  _MockAvatarDetectorRepository() {
+    when(preloadLandmarksModel).thenAnswer((_) => Future.value());
+    // ignore: inference_failure_on_function_invocation
+    when(() => detectFace(any())).thenAnswer((_) async => null);
+  }
+}
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(
     Widget widget, {
     PhotosRepository? photosRepository,
+    AvatarDetectorRepository? avatarDetectorRepository,
   }) async {
     return mockNetworkImages(() async {
-      return pumpWidget(
-        RepositoryProvider.value(
-          value: photosRepository ?? MockPhotosRepository(),
-          child: MaterialApp(
-            localizationsDelegates: const [
-              AppLocalizations.delegate,
-              GlobalMaterialLocalizations.delegate,
-            ],
-            supportedLocales: AppLocalizations.supportedLocales,
-            home: widget,
+      return pumpWidget(MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider.value(
+            value: photosRepository ?? _MockPhotosRepository(),
           ),
+          RepositoryProvider.value(
+            value: avatarDetectorRepository ?? _MockAvatarDetectorRepository(),
+          ),
+        ],
+        child: MaterialApp(
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+          ],
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: widget,
         ),
-      );
+      ));
     });
   }
 }
