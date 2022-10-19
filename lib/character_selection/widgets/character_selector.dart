d740687f5d96a89fba5cbb22a8ffa3218e04a2d7
alestiago <dev@alestiago.com>
2022-10-19 19:04:09 +0100

    feat: improved layout

--- lib/character_selection/widgets/character_selector.dart
+++ lib/character_selection/widgets/character_selector.dart
@@ -6,46 +6,23 @@ class CharacterSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final characters = [
-      _Character(
-        name: 'Dash',
-        image: Assets.characters.dash.image(),
-      ),
-      _Character(
-        name: 'Sparky',
-        image: Assets.characters.sparky.image(),
-      ),
-    ];
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        print(constraints.maxWidth);
-
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-              flex: 2,
-              child: _Character(
-                name: 'Dash',
-                image: Assets.characters.dash.image(),
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: ColoredBox(color: Colors.red),
-            ),
-            Flexible(
-              flex: 2,
-              child: _Character(
-                name: 'Sparky',
-                image: Assets.characters.sparky.image(),
-              ),
-            ),
-          ],
-        );
-      },
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: _Character(
+            name: 'Dash',
+            image: Assets.characters.dash.image(),
+          ),
+        ),
+        const SizedBox(width: 42),
+        Flexible(
+          child: _Character(
+            name: 'Sparky',
+            image: Assets.characters.sparky.image(),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -66,20 +43,14 @@ class _Character extends StatelessWidget {
 
     return Column(
       children: [
-        SizedBox.fromSize(
-          size: Size.square(300),
+        SizedBox(
+          width: 300,
           child: image,
         ),
         const SizedBox(height: 35.67),
         SelectableText(
           name,
           style: theme.textTheme.headline1,
-          textAlign: TextAlign.center,
-        ),
-        const SizedBox(height: 11),
-        SelectableText(
-          'You can change them later',
-          style: theme.textTheme.headline3,
           textAlign: TextAlign.center,
         ),
       ],
