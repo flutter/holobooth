0990bd1614838cd08e309a55f76d02cdf9bc5a3e
alestiago <dev@alestiago.com>
2022-10-24 09:42:21 +0100

    test: used pumpSubject

--- test/character_selection/view/character_selection_page_test.dart
+++ test/character_selection/view/character_selection_page_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_photobooth/character_selection/character_selection.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
 
 import '../../helpers/helpers.dart';
 
@@ -17,14 +18,31 @@ void main() {
     group('renders', () {
       testWidgets('successfully', (tester) async {
         final subject = CharacterSelectionPage();
-        await tester.pumpApp(subject);
+        await tester.pumpSubject(subject);
         expect(find.byWidget(subject), findsOneWidget);
       });
 
       testWidgets('a CharacterSelecionView', (tester) async {
-        await tester.pumpApp(const CharacterSelectionPage());
+        await tester.pumpSubject(const CharacterSelectionPage());
         expect(find.byType(CharacterSelectionView), findsOneWidget);
       });
     });
   });
+}
+
+extension on WidgetTester {
+  Future<void> pumpSubject(
+    CharacterSelectionPage subject,
+  ) =>
+      pumpWidget(
+        MediaQuery.fromWindow(
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Theme(
+              data: PhotoboothTheme.standard,
+              child: subject,
+            ),
+          ),
+        ),
+      );
 }
