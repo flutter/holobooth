d7da3c0151d7bb62836a40d81043234897067dee
arturplaczek <arturp1989@gmail.com>
2022-10-25 12:40:36 +0200

    feat: create MouthGeometry

---
+++ packages/face_geometry/lib/src/models/mouth_geometry.dart
@@ -0,0 +1,49 @@
+import 'package:face_geometry/face_geometry.dart';
+import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
+
+/// An eyeballed estimated minimum mouth to face ratio.
+///
+/// This represents the minimum height of the mouth in comparison to the face
+/// height.
+const _minMouthFaceRatio = 0.02;
+
+/// {@template mouth_geometry}
+/// An object which holds data for eye blink.
+/// {@endtemplate}
+class MouthGeometry extends BaseGeometry {
+  /// {@macro mouth_geometry}
+  MouthGeometry(super.keypoints, super.boundingBox);
+
+  /// The distance between the top lip and the bottom lip.
+  double get mouthDistance {
+    if (keypoints.length < 15) return 0;
+    final topLipPoint = keypoints[13];
+    final bottomLipPoint = keypoints[14];
+    return topLipPoint.distanceTo(bottomLipPoint);
+  }
+
+  /// Defines if the mouth is open based on hight and face height.
+  bool get isMouthOpen {
+    if (mouthDistance > 1) {
+      if (boundingBox.height == 0) return false;
+      final heightRatio = mouthDistance / boundingBox.height;
+      if (heightRatio == 0) return false;
+
+      return heightRatio > _minMouthFaceRatio;
+    }
+
+    return false;
+  }
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+
+  @override
+  void update(
+    List<Keypoint> newKeypoints,
+    BoundingBox newBoundingBox,
+  ) {
+    keypoints = newKeypoints;
+    boundingBox = newBoundingBox;
+  }
+}
---
+++ packages/face_geometry/test/src/models/mouth_geometry_test.dart
@@ -0,0 +1,111 @@
+import 'dart:collection';
+
+import 'package:face_geometry/face_geometry.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
+import 'package:test/test.dart';
+
+class _MockFace extends Mock implements Face {}
+
+class _MockBoundingBox extends Mock implements BoundingBox {}
+
+class _FakeKeypoint extends Fake implements Keypoint {
+  _FakeKeypoint(this.x, this.y);
+
+  @override
+  final double x;
+
+  @override
+  final double y;
+}
+
+void main() {
+  late Face face;
+  late FaceGeometry faceGeometry;
+  late BoundingBox boundingBox;
+
+  setUp(() {
+    boundingBox = _MockBoundingBox();
+    face = _MockFace();
+
+    when(() => boundingBox.height).thenReturn(100);
+    when(() => face.boundingBox).thenReturn(boundingBox);
+
+    faceGeometry = FaceGeometry(face);
+  });
+
+  group('MouthGeometry', () {
+    group('mouthDistance', () {
+      test('returns normally', () {
+        final keypoints = UnmodifiableListView(
+          List.generate(15, (_) => _FakeKeypoint(0, 0)),
+        );
+        when(() => face.keypoints).thenReturn(keypoints);
+
+        expect(() => faceGeometry.mouth.mouthDistance, returnsNormally);
+      });
+
+      group('returns correct distance', () {
+        test('when values are 0', () {
+          final keypoints = UnmodifiableListView(
+            List.generate(15, (_) => _FakeKeypoint(0, 0)),
+          );
+          when(() => face.keypoints).thenReturn(keypoints);
+
+          expect(faceGeometry.mouth.mouthDistance, equals(0));
+        });
+
+        test('when values are not 0', () {
+          final keypoints = List.generate(15, (_) => _FakeKeypoint(0, 0));
+          keypoints[13] = _FakeKeypoint(-2, 2);
+          keypoints[14] = _FakeKeypoint(1, -1);
+          when(() => face.keypoints)
+              .thenReturn(UnmodifiableListView(keypoints));
+
+          expect(
+            faceGeometry.mouth.mouthDistance,
+            equals(4.242640687119285),
+          );
+        });
+      });
+    });
+
+    group('isMouthOpen', () {
+      setUp(() {
+        final boundingBox = _MockBoundingBox();
+        when(() => boundingBox.height).thenReturn(100);
+        when(() => face.boundingBox).thenReturn(boundingBox);
+      });
+
+      test('returns normally', () {
+        final keypoints = UnmodifiableListView(
+          List.generate(15, (_) => _FakeKeypoint(0, 0)),
+        );
+        when(() => face.keypoints).thenReturn(keypoints);
+
+        expect(() => faceGeometry.mouth.isMouthOpen, returnsNormally);
+      });
+
+      group('returns correct distance', () {
+        test('when values are 0', () {
+          final keypoints = UnmodifiableListView(
+            List.generate(15, (_) => _FakeKeypoint(0, 0)),
+          );
+          when(() => face.keypoints).thenReturn(keypoints);
+
+          expect(faceGeometry.mouth.isMouthOpen, equals(false));
+        });
+
+        test('when values are not 0', () {
+          final keypoints = List.generate(15, (_) => _FakeKeypoint(0, 0));
+          keypoints[13] = _FakeKeypoint(-2, 2);
+          keypoints[14] = _FakeKeypoint(1, -1);
+          when(() => face.keypoints)
+              .thenReturn(UnmodifiableListView(keypoints));
+
+          expect(faceGeometry.mouth.isMouthOpen, equals(true));
+        });
+      });
+    });
+  });
+}
