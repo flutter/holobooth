da2c8b1418135fe3157e1997a1e9d4cb312b57ae
Oscar <martinm.oscar@gmail.com>
2022-10-25 10:43:11 +0200

    chore: mocks

--- test/avatar_animation/widgets/dash_animation_test.dart
+++ test/avatar_animation/widgets/dash_animation_test.dart
@@ -1,8 +1,54 @@
 import 'package:avatar_detector_repository/avatar_detector_repository.dart';
 import 'package:face_geometry/face_geometry.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_photobooth/avatar_animation/avatar_animation.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:rive/rive.dart';
+
+class _MockSMINumber extends Mock implements SMINumber {
+  @override
+  bool change(double value) {
+    this.value = value;
+    return true;
+  }
+}
+
+class _MockSMIBool extends Mock implements SMIBool {
+  _MockSMIBool(this.value);
+  @override
+  bool change(bool value) {
+    this.value = value;
+    return true;
+  }
+
+  @override
+  final bool value;
+}
+
+class _MockDashStateMachineController extends Mock
+    implements DashStateMachineController {
+  @override
+  SMINumber get x => _MockSMINumber();
+  @override
+  SMINumber get y => _MockSMINumber();
+  @override
+  SMIBool get openMouth => _MockSMIBool(true);
+
+  @override
+  bool init(CoreContext core) {
+    return true;
+  }
+
+  @override
+  ValueListenable<bool> get isActiveChanged => ValueNotifier(false);
+
+  @override
+  bool get isActive => true;
+}
+
+class MockArtboard extends Mock implements Artboard {}
 
 void main() {
   group('DashAnimation', () {
