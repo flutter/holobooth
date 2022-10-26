c2baa4c246b37e8d7567bb0f576491642b33731a
alestiago <dev@alestiago.com>
2022-10-26 09:55:05 +0100

    test: included Localizations

--- test/character_selection/view/character_selection_page_test.dart
+++ test/character_selection/view/character_selection_page_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_photobooth/character_selection/character_selection.dart';
+import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
 void main() {
@@ -36,9 +38,17 @@ extension on WidgetTester {
         MediaQuery.fromWindow(
           child: Directionality(
             textDirection: TextDirection.ltr,
-            child: Theme(
-              data: PhotoboothTheme.standard,
-              child: subject,
+            child: Localizations(
+              locale: Locale('en'),
+              delegates: const [
+                AppLocalizations.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+              ],
+              child: Theme(
+                data: PhotoboothTheme.standard,
+                child: subject,
+              ),
             ),
           ),
         ),
