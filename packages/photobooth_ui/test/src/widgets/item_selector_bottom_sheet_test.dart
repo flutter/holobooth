c9417ad1481bda4695e4150ffbd3670aaae315d3
Laura177 <laura.c.buffington@gmail.com>
2022-10-27 09:45:43 -0700

    test: show bottom sheet method

--- packages/photobooth_ui/test/src/widgets/item_selector_bottom_sheet_test.dart
+++ packages/photobooth_ui/test/src/widgets/item_selector_bottom_sheet_test.dart
@@ -37,5 +37,44 @@ void main() {
         expect(find.text(item), findsOneWidget);
       }
     });
+
+    testWidgets('calls onSelected when item is tapped', (tester) async {
+      await tester.pumpWidget(buildSubject());
+      for (final item in items) {
+        await tester.ensureVisible(find.text(item));
+        await tester.dragUntilVisible(
+          find.text(item),
+          find.byType(ClipRRect),
+          const Offset(-200, 0),
+        );
+        await tester.tapAt(tester.getTopLeft(find.text(item)));
+        expect(selectedItem, equals(item));
+      }
+    });
+
+    testWidgets('show renders bottomSheet', (tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: Builder(
+              builder: (context) => TextButton(
+                onPressed: () => ItemSelectorBottomSheet.show(
+                  context,
+                  title: title,
+                  items: items,
+                  itemBuilder: itemBuilder,
+                  selectedItem: selectedItem,
+                  onSelected: (val) {},
+                ),
+                child: const Text('show bottom sheet'),
+              ),
+            ),
+          ),
+        ),
+      );
+      await tester.tap(find.text('show bottom sheet'));
+      await tester.pumpAndSettle();
+      expect(find.byType(ItemSelectorBottomSheet<String>), findsOneWidget);
+    });
   });
 }
