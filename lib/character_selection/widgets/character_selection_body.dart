f292a4779c3cff0cbbfa46da8113c017931f5ddb
Oscar <martinm.oscar@gmail.com>
2022-10-27 20:28:33 +0200

    chore: remove padding

--- lib/character_selection/widgets/character_selection_body.dart
+++ lib/character_selection/widgets/character_selection_body.dart
@@ -12,60 +12,44 @@ class CharacterSelectionBody extends StatelessWidget {
     final theme = Theme.of(context);
     final l10n = context.l10n;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          const SizedBox(height: 104),
-          SelectableText(
-            l10n.chooseYourCharacterTitleText,
-            style: theme.textTheme.headline1,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 24),
-          SelectableText(
-            l10n.youCanChangeThemLaterSubheading,
-            style: theme.textTheme.headline3,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 53),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              if (constraints.maxWidth <= PhotoboothBreakpoints.small) {
-                return Text('Small');
-              } else if (constraints.maxWidth <= PhotoboothBreakpoints.medium) {
-                return Text('Medium');
-              } else if (constraints.maxWidth <= PhotoboothBreakpoints.large) {
-                return Text('Large');
-              } else {
-                return Text('XLarge');
-              }
-            },
-          ),
-          LayoutBuilder(
-            builder: (context, constraints) {
-              Breakpoint breakpoint;
-              if (constraints.maxWidth <= PhotoboothBreakpoints.small) {
-                breakpoint = Breakpoint.small;
-              } else if (constraints.maxWidth <= PhotoboothBreakpoints.medium) {
-                breakpoint = Breakpoint.medium;
-              } else if (constraints.maxWidth <= PhotoboothBreakpoints.large) {
-                breakpoint = Breakpoint.large;
-              } else {
-                breakpoint = Breakpoint.xLarge;
-              }
-              return CharacterSelector(breakpoint: breakpoint);
-            },
-          ),
-          const SizedBox(height: 42),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.of(context).push(MultipleCapturePage.route());
-            },
-            child: const Icon(Icons.arrow_right),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        const SizedBox(height: 104),
+        SelectableText(
+          l10n.chooseYourCharacterTitleText,
+          style: theme.textTheme.headline1,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 24),
+        SelectableText(
+          l10n.youCanChangeThemLaterSubheading,
+          style: theme.textTheme.headline3,
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 53),
+        LayoutBuilder(
+          builder: (context, constraints) {
+            Breakpoint breakpoint;
+            if (constraints.maxWidth <= PhotoboothBreakpoints.small) {
+              breakpoint = Breakpoint.small;
+            } else if (constraints.maxWidth <= PhotoboothBreakpoints.medium) {
+              breakpoint = Breakpoint.medium;
+            } else if (constraints.maxWidth <= PhotoboothBreakpoints.large) {
+              breakpoint = Breakpoint.large;
+            } else {
+              breakpoint = Breakpoint.xLarge;
+            }
+            return CharacterSelector(breakpoint: breakpoint);
+          },
+        ),
+        const SizedBox(height: 42),
+        FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(MultipleCapturePage.route());
+          },
+          child: const Icon(Icons.arrow_right),
+        ),
+      ],
     );
   }
 }
