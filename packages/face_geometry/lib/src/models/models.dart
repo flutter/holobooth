2296e6adb4a6007e376d0fd32c0b11d6272842f8
alestiago <dev@alestiago.com>
2022-10-31 10:10:50 +0000

    refactor: normalization

--- packages/face_geometry/lib/src/models/models.dart
+++ packages/face_geometry/lib/src/models/models.dart
@@ -2,4 +2,5 @@ export 'base_geometry.dart';
 export 'eye_geometry.dart';
 export 'face_direction.dart';
 export 'mouth_geometry.dart';
+export 'normalization.dart';
 export 'vector3.dart';
--- packages/face_geometry/test/src/face_geometry_test.dart
+++ packages/face_geometry/test/src/face_geometry_test.dart
@@ -4,7 +4,6 @@ import 'package:face_geometry/face_geometry.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart'
     as tf;
-import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart';
 import 'package:test/test.dart';
 
 class _MockKeypoint extends Mock implements tf.Keypoint {}
@@ -13,7 +12,7 @@ class _MockBoundingBox extends Mock implements tf.BoundingBox {}
 
 void main() {
   setUpAll(() {
-    registerFallbackValue(Size(0, 0));
+    registerFallbackValue(tf.Size(0, 0));
   });
 
   group('FaceGeometry', () {
@@ -44,7 +43,7 @@ void main() {
     });
 
     test('update changes FaceGeometry', () {
-      final newKeypoints = UnmodifiableListView(<Keypoint>[_MockKeypoint()]);
+      final newKeypoints = UnmodifiableListView(<tf.Keypoint>[_MockKeypoint()]);
       final faceGeometry = FaceGeometry(const [], _MockBoundingBox());
       final faceGeometry2 = FaceGeometry(const [], _MockBoundingBox());
 
@@ -52,137 +51,6 @@ void main() {
 
       expect(faceGeometry.keypoints, equals(newKeypoints));
       expect(faceGeometry, isNot(faceGeometry2));
-    });
-
-    group('normalize', () {
-      final keypoint = tf.Keypoint.fromJson({'x': 10, 'y': 10});
-      final boundingBox = tf.BoundingBox.fromJson({
-        'xMin': 10,
-        'yMin': 10,
-        'xMax': 30,
-        'yMax': 30,
-        'width': 20,
-        'height': 20,
-      });
-
-      group('the number', () {
-        test('throws assertion when fromMax is equals 0', () {
-          expect(
-            () => 0.normalize(fromMax: 0, toMax: 1),
-            throwsA(isA<AssertionError>()),
-          );
-        });
-
-        test('to greater values', () {
-          expect(0.5.normalize(fromMax: 1, toMax: 3), equals(1.5));
-          expect(0.5.normalize(fromMax: 2, toMax: 3), equals(0.75));
-          expect(0.5.normalize(fromMax: 2, toMax: 4), equals(1));
-        });
-
-        test('to lower values', () {
-          expect(0.5.normalize(fromMax: 4, toMax: 1), equals(0.125));
-          expect(0.5.normalize(fromMax: 3, toMax: 2), equals(1 / 3));
-          expect(0.5.normalize(fromMax: 4, toMax: 2), equals(0.25));
-        });
-      });
-
-      group('the keypoint', () {
-        test('to greater value', () {
-          expect(
-            keypoint.normalize(
-              fromMax: Size(20, 30),
-              toMax: Size(40, 60),
-            ),
-            isA<tf.Keypoint>()
-                .having((keypoint) => keypoint.x, 'x', 20)
-                .having((keypoint) => keypoint.y, 'y', 20),
-          );
-        });
-        test('to lower value', () {
-          expect(
-            keypoint.normalize(
-              fromMax: Size(40, 60),
-              toMax: Size(20, 30),
-            ),
-            isA<tf.Keypoint>()
-                .having((keypoint) => keypoint.x, 'x', 5)
-                .having((keypoint) => keypoint.y, 'y', 5),
-          );
-        });
-      });
-
-      group('the BoundingBox', () {
-        test('to greater value', () {
-          expect(
-            boundingBox.normalize(
-              fromMax: Size(20, 30),
-              toMax: Size(40, 60),
-            ),
-            isA<tf.BoundingBox>()
-                .having((boundingBox) => boundingBox.height, 'width', 40)
-                .having((boundingBox) => boundingBox.width, 'height', 40)
-                .having((boundingBox) => boundingBox.xMax, 'xMax', 60)
-                .having((boundingBox) => boundingBox.xMin, 'xMin', 20)
-                .having((boundingBox) => boundingBox.yMax, 'yMax', 60)
-                .having((boundingBox) => boundingBox.yMin, 'yMin', 20),
-          );
-        });
-
-        test('to lower value', () {
-          expect(
-            boundingBox.normalize(
-              fromMax: Size(40, 60),
-              toMax: Size(20, 30),
-            ),
-            isA<tf.BoundingBox>()
-                .having((boundingBox) => boundingBox.height, 'width', 10)
-                .having((boundingBox) => boundingBox.width, 'height', 10)
-                .having((boundingBox) => boundingBox.xMax, 'xMax', 15)
-                .having((boundingBox) => boundingBox.xMin, 'xMin', 5)
-                .having((boundingBox) => boundingBox.yMax, 'yMax', 15)
-                .having((boundingBox) => boundingBox.yMin, 'yMin', 5),
-          );
-        });
-      });
-
-      group('the Face', () {
-        final face = tf.Face(
-          UnmodifiableListView([keypoint]),
-          boundingBox,
-        );
-
-        test('to greater value', () {
-          final normalizedFace = face.normalize(
-            fromMax: Size(20, 30),
-            toMax: Size(40, 60),
-          );
-
-          expect(normalizedFace.keypoints.first.x, 20);
-          expect(normalizedFace.keypoints.first.y, 20);
-          expect(normalizedFace.boundingBox.xMin, 20);
-          expect(normalizedFace.boundingBox.yMin, 20);
-          expect(normalizedFace.boundingBox.xMax, 60);
-          expect(normalizedFace.boundingBox.yMax, 60);
-          expect(normalizedFace.boundingBox.width, 40);
-          expect(normalizedFace.boundingBox.height, 40);
-        });
-
-        test('to lower value', () {
-          final normalizedFace = face.normalize(
-            fromMax: Size(40, 60),
-            toMax: Size(20, 30),
-          );
-
-          expect(normalizedFace.keypoints.first.x, 5);
-          expect(normalizedFace.keypoints.first.y, 5);
-          expect(normalizedFace.boundingBox.xMin, 5);
-          expect(normalizedFace.boundingBox.yMin, 5);
-          expect(normalizedFace.boundingBox.xMax, 15);
-          expect(normalizedFace.boundingBox.yMax, 15);
-          expect(normalizedFace.boundingBox.width, 10);
-          expect(normalizedFace.boundingBox.height, 10);
-        });
-      });
     });
   });
 }
