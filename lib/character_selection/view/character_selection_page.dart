b60dcb7846d5719f8ae9f3748b27bb47ef94de4f
Oscar <martinm.oscar@gmail.com>
2022-10-31 17:40:44 +0100

    chore: rollback

--- lib/character_selection/view/character_selection_page.dart
+++ lib/character_selection/view/character_selection_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:io_photobooth/assets/assets.dart';
 import 'package:io_photobooth/character_selection/character_selection.dart';
 import 'package:io_photobooth/footer/footer.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
@@ -25,39 +24,10 @@ class CharacterSelectionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppPageView(
+    return const AppPageView(
       background: CharacterSelectionBackground(),
-      background1: CharacterSelectionBackground1(),
       body: CharacterSelectionBody(),
       footer: SimplifiedFooter(),
-    );
-  }
-}
-
-class LightOverlay extends StatelessWidget {
-  const LightOverlay({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (p0, constraints) {
-        double widthFactor;
-        if (constraints.maxWidth <= PhotoboothBreakpoints.small) {
-          widthFactor = 0.6;
-        } else if (constraints.maxWidth <= PhotoboothBreakpoints.medium) {
-          widthFactor = 0.6;
-        } else {
-          widthFactor = 0.35;
-        }
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Opacity(
-            opacity: 0.5,
-            child: Assets.backgrounds.lightEffectBackground
-                .image(width: constraints.maxWidth * widthFactor),
-          ),
-        );
-      },
     );
   }
 }
