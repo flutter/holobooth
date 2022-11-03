9a59cd2ac27114cd16d776a14ef6e77d60ca149c
Oscar <martinm.oscar@gmail.com>
2022-11-03 13:40:42 +0100

    chore: size

--- lib/footer/widgets/footer.dart
+++ lib/footer/widgets/footer.dart
@@ -27,13 +27,16 @@ class Footer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: Theme.of(context).textTheme.caption!.copyWith(color: textColor),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(50, 0, 50, 32),
-        child: ResponsiveLayoutBuilder(
-          small: (_, __) => const _ColumnFooter(key: Key('footer_column')),
-          large: (_, __) => const _RowFooter(key: Key('footer_row')),
+    return SizedBox(
+      height: 100,
+      child: DefaultTextStyle(
+        style: Theme.of(context).textTheme.caption!.copyWith(color: textColor),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(50, 0, 50, 32),
+          child: ResponsiveLayoutBuilder(
+            small: (_, __) => const _ColumnFooter(key: Key('footer_column')),
+            large: (_, __) => const _RowFooter(key: Key('footer_row')),
+          ),
         ),
       ),
     );
