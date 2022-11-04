7eee7c886b2a5a9b8a68e4cc81363b2e318e9ec1
Alejandro Santiago <dev@alestiago.com>
2022-11-04 08:29:01 +0000

    chore: removes LandmarksSingleImagePage example (#178)

--- packages/tensorflow_models/tensorflow_models/example/lib/src/pages/pages.dart
+++ packages/tensorflow_models/tensorflow_models/example/lib/src/pages/pages.dart
@@ -2,5 +2,4 @@ export 'landmarks_dash_page.dart';
 export 'landmarks_detect_blink_page.dart';
 export 'landmarks_gif_page.dart';
 export 'landmarks_open_mouth_page.dart';
-export 'landmarks_single_image_page.dart';
 export 'landmarks_video_stream_page.dart';
