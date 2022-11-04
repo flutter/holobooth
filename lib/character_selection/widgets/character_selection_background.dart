20d02914723bfa8ce3766740ae1d877286f4c96d
Oscar <martinm.oscar@gmail.com>
2022-11-04 12:35:17 +0100

    chore: background

--- lib/character_selection/widgets/character_selection_background.dart
+++ lib/character_selection/widgets/character_selection_background.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
 
 class CharacterSelectionBackground extends StatelessWidget {
   const CharacterSelectionBackground({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DecoratedBox(
+    return const DecoratedBox(
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            const Color(0xffBB42F4).withOpacity(1),
-            const Color(0xff428eff).withOpacity(1),
+            PhotoboothColors.purple,
+            PhotoboothColors.blue,
           ],
         ),
       ),
---
+++ test/character_selection/widgets/character_selection_background_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('character selection background ...', (tester) async {
+    // TODO: Implement test
+  });
+}
