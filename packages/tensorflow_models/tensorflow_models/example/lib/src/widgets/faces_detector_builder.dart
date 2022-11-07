341c875e3e6562ced6fd9f81dab3bde97ac36b1d
alestiago <dev@alestiago.com>
2022-11-07 14:54:41 +0000

    feat: included FaceGeometryOverlay

--- packages/tensorflow_models/tensorflow_models/example/lib/src/widgets/faces_detector_builder.dart
+++ packages/tensorflow_models/tensorflow_models/example/lib/src/widgets/faces_detector_builder.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:camera/camera.dart';
 import 'package:face_geometry/face_geometry.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tensorflow_models/tensorflow_models.dart' as tf;
 
@@ -10,10 +11,12 @@ class FacesDetectorBuilder extends StatefulWidget {
     Key? key,
     required this.cameraController,
     required this.builder,
+    this.showFaceGeometryOverlay = kDebugMode,
   }) : super(key: key);
 
   final CameraController cameraController;
   final Widget Function(BuildContext context, tf.Faces faces) builder;
+  final bool showFaceGeometryOverlay;
 
   @override
   State<FacesDetectorBuilder> createState() => _FacesDetectorBuilderState();
@@ -23,6 +26,7 @@ class _FacesDetectorBuilderState extends State<FacesDetectorBuilder> {
   final _streamController = StreamController<tf.Faces>();
   late final tf.FaceLandmarksDetector _faceLandmarksDetector;
   late Size _size;
+  FaceGeometry? _faceGeometry;
 
   static const _estimationConfig = tf.EstimationConfig(
     flipHorizontal: true,
@@ -55,7 +59,15 @@ class _FacesDetectorBuilderState extends State<FacesDetectorBuilder> {
       fromMax: imageData.size,
       toMax: tf.Size(_size.width.toInt(), _size.height.toInt()),
     );
-    if (!_streamController.isClosed) _streamController.add(faces);
+    if (!_streamController.isClosed) {
+      if (faces.isNotEmpty && widget.showFaceGeometryOverlay) {
+        _faceGeometry = _faceGeometry == null
+            ? FaceGeometry.fromFace(faces.first)
+            : _faceGeometry!.update(faces.first);
+      }
+
+      _streamController.add(faces);
+    }
     return !_streamController.isClosed;
   }
 
@@ -82,7 +94,14 @@ class _FacesDetectorBuilderState extends State<FacesDetectorBuilder> {
 
               final data = snapshot.data;
               if (data != null) {
-                return widget.builder(context, data);
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    widget.builder(context, data),
+                    if (widget.showFaceGeometryOverlay && _faceGeometry != null)
+                      _FaceGeometryOverlay(faceGeometry: _faceGeometry!),
+                  ],
+                );
               } else {
                 return const SizedBox.shrink();
               }
@@ -90,6 +109,27 @@ class _FacesDetectorBuilderState extends State<FacesDetectorBuilder> {
           );
         },
       ),
+    );
+  }
+}
+
+class _FaceGeometryOverlay extends StatelessWidget {
+  const _FaceGeometryOverlay({
+    Key? key,
+    required this.faceGeometry,
+  }) : super(key: key);
+
+  final FaceGeometry faceGeometry;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Right Eye: ${faceGeometry.rightEye.isClosed ? 'Closed' : 'Open'}\n'
+      'Left Eye: ${faceGeometry.leftEye.isClosed ? 'Closed' : 'Open'}\n'
+      'Mouth: ${faceGeometry.mouth.isOpen ? 'Open' : 'Closed'}\n'
+      'Direction X: ${faceGeometry.direction.value.x}\n'
+      'Direction Y: ${faceGeometry.direction.value.y}\n'
+      'Direction Z: ${faceGeometry.direction.value.z}\n',
     );
   }
 }
