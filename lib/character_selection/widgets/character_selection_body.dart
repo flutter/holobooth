92ca3e659168da2f4144075368a4af737de0042e
Oscar <martinm.oscar@gmail.com>
2022-11-08 10:51:35 +0100

    chore: refactor body

--- lib/character_selection/widgets/character_selection_body.dart
+++ lib/character_selection/widgets/character_selection_body.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:io_photobooth/character_selection/character_selection.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
@@ -14,12 +12,16 @@ class CharacterSelectionBody extends StatelessWidget {
     final l10n = context.l10n;
     final size = MediaQuery.of(context).size;
     double bodyHeight;
+    double viewPortFraction;
     if (size.width <= PhotoboothBreakpoints.small) {
       bodyHeight = size.height * 0.6;
+      viewPortFraction = 0.55;
     } else if (size.width <= PhotoboothBreakpoints.medium) {
       bodyHeight = size.height * 0.6;
+      viewPortFraction = 0.3;
     } else {
       bodyHeight = size.height * 0.75;
+      viewPortFraction = 0.2;
     }
 
     return Column(
@@ -56,30 +58,9 @@ class CharacterSelectionBody extends StatelessWidget {
               // Light shadow
               Align(
                 alignment: Alignment.topCenter,
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    double viewPortFraction;
-                    if (constraints.maxWidth <= PhotoboothBreakpoints.small) {
-                      viewPortFraction = 0.55;
-                    } else if (constraints.maxWidth <=
-                        PhotoboothBreakpoints.medium) {
-                      viewPortFraction = 0.3;
-                    } else {
-                      viewPortFraction = 0.2;
-                    }
-                    return SizedBox(
-                      width: size.width * viewPortFraction * 1.4,
-                      height: bodyHeight,
-                      child: CustomPaint(
-                        size: MediaQuery.of(context).size,
-                        painter: DrawTriangle(),
-                        child: CustomPaint(
-                          size: MediaQuery.of(context).size,
-                          painter: DrawBase(),
-                        ),
-                      ),
-                    );
-                  },
+                child: CharacterSpotlight(
+                  bodyHeight: bodyHeight,
+                  viewPortFraction: viewPortFraction,
                 ),
               ),
               // Character selection
@@ -106,64 +87,5 @@ class CharacterSelectionBody extends StatelessWidget {
         ),
       ],
     );
-  }
-}
-
-class DrawTriangle extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final pathLightBody = Path()
-      ..moveTo(size.width / 3, 0)
-      ..lineTo(0, size.height)
-      ..lineTo(size.width, size.height)
-      ..lineTo((size.width / 3) * 2, 0)
-      ..close();
-    final paintLightBody = Paint()
-      ..blendMode = BlendMode.overlay
-      ..shader = LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          PhotoboothColors.white.withOpacity(0.6),
-          PhotoboothColors.white.withOpacity(0),
-        ],
-      ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
-    canvas.drawPath(pathLightBody, paintLightBody);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
-  }
-}
-
-class DrawBase extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final center = Offset(size.width / 2, size.height);
-    canvas.save();
-    // Rotation of the canvas to paint the circle and we rotate to get
-    //the oval effect
-    final matrix = Matrix4.identity()
-      ..translate(center.dx, center.dy)
-      ..rotateX(pi * 0.43);
-    canvas.transform(matrix.storage);
-    final paint = Paint()
-      ..shader = RadialGradient(
-        colors: [
-          const Color(0XFFF4E4E4).withOpacity(0.5),
-          const Color(0XFFF4E4E4).withOpacity(0)
-        ],
-      ).createShader(
-        Rect.fromCircle(center: Offset.zero, radius: size.width / 2),
-      );
-    canvas
-      ..drawCircle(Offset.zero, size.width / 2, paint)
-      ..restore();
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
   }
 }
--- lib/character_selection/widgets/widgets.dart
+++ lib/character_selection/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'character_selection_background.dart';
 export 'character_selection_body.dart';
 export 'character_selector.dart';
+export 'character_spotlight.dart';
 export 'next_button.dart';
