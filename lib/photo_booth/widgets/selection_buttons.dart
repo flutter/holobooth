a29a94d9d9d318dfcc9ceed14f7818f51e080e9c
Oscar <martinm.oscar@gmail.com>
2022-11-14 17:12:03 +0100

    feat: open background picker automatically (#190)
    * feat: open background picker automatically
    
    * chore: format

--- lib/photo_booth/widgets/selection_buttons.dart
+++ lib/photo_booth/widgets/selection_buttons.dart
@@ -11,34 +11,27 @@ class SelectionButtons extends StatelessWidget {
   static const charactersSelectionButtonKey = Key('itemSelector_characters');
 
   @visibleForTesting
+  static const characterSelectionBottomSheetKey =
+      Key('selectionButtons_characters_bottomSheet');
+
+  @visibleForTesting
   static const propsSelectionButtonKey = Key('itemSelector_props');
+
+  @visibleForTesting
+  static const propsSelectionBottomSheetKey =
+      Key('selectionButtons_props_bottomSheet');
 
   @visibleForTesting
   static const backgroundSelectorButtonKey = Key('itemSelector_background');
 
-  void _showSelector(BuildContext context, DrawerOption drawerOption) {
-    context.read<DrawerSelectionBloc>().add(
-          DrawerSelectionOptionSelected(
-            drawerOption: drawerOption,
-          ),
-        );
-    if (MediaQuery.of(context).size.width >= PhotoboothBreakpoints.small) {
-      return Scaffold.of(context).openEndDrawer();
-    } else {
-      switch (drawerOption) {
-        case DrawerOption.characters:
-          return _showCharacterBottomSheet(context);
-        case DrawerOption.props:
-          return _showPropsBottomSheet(context);
-        case DrawerOption.backgrounds:
-          return _showBackgroundBottomSheet(context);
-      }
-    }
-  }
+  @visibleForTesting
+  static const backgroundSelectionBottomSheetKey =
+      Key('selectionButtons_background_bottomSheet');
 
   void _showCharacterBottomSheet(BuildContext context) {
     ItemSelectorBottomSheet.show<Color>(
       context,
+      key: characterSelectionBottomSheetKey,
       title: context.l10n.characterSelectorButton,
       items: const [Colors.red, Colors.yellow, Colors.green],
       itemBuilder: (context, item) => ColoredBox(color: item),
@@ -50,6 +43,7 @@ class SelectionButtons extends StatelessWidget {
   void _showPropsBottomSheet(BuildContext context) {
     ItemSelectorBottomSheet.show<Color>(
       context,
+      key: propsSelectionBottomSheetKey,
       title: context.l10n.propsSelectorButton,
       items: const [Colors.red, Colors.yellow, Colors.green],
       itemBuilder: (context, item) => ColoredBox(color: item),
@@ -61,6 +55,7 @@ class SelectionButtons extends StatelessWidget {
   void _showBackgroundBottomSheet(BuildContext context) {
     ItemSelectorBottomSheet.show<Color>(
       context,
+      key: backgroundSelectionBottomSheetKey,
       title: context.l10n.backgroundSelectorButton,
       items: const [Colors.red, Colors.yellow, Colors.green],
       itemBuilder: (context, item) => ColoredBox(color: item),
@@ -77,40 +72,70 @@ class SelectionButtons extends StatelessWidget {
       height: screenSize >= PhotoboothBreakpoints.small ? 48 : 0,
     );
 
-    return Align(
-      alignment: screenSize >= PhotoboothBreakpoints.small
-          ? Alignment.centerRight
-          : Alignment.topCenter,
-      child: Flex(
-        mainAxisSize: MainAxisSize.min,
-        direction: screenSize >= PhotoboothBreakpoints.small
-            ? Axis.vertical
-            : Axis.horizontal,
-        children: [
-          ItemSelectorButton(
-            key: SelectionButtons.charactersSelectionButtonKey,
-            buttonBackground: const ColoredBox(color: Colors.red),
-            title: context.l10n.characterSelectorButton,
-            showTitle: screenSize >= PhotoboothBreakpoints.small,
-            onTap: () => _showSelector(context, DrawerOption.characters),
-          ),
-          spacer,
-          ItemSelectorButton(
-            key: SelectionButtons.propsSelectionButtonKey,
-            buttonBackground: const ColoredBox(color: Colors.red),
-            title: context.l10n.propsSelectorButton,
-            showTitle: screenSize >= PhotoboothBreakpoints.small,
-            onTap: () => _showSelector(context, DrawerOption.props),
-          ),
-          spacer,
-          ItemSelectorButton(
-            key: SelectionButtons.backgroundSelectorButtonKey,
-            buttonBackground: const ColoredBox(color: Colors.red),
-            title: context.l10n.backgroundSelectorButton,
-            showTitle: screenSize >= PhotoboothBreakpoints.small,
-            onTap: () => _showSelector(context, DrawerOption.backgrounds),
-          ),
-        ],
+    return BlocListener<DrawerSelectionBloc, DrawerSelectionState>(
+      listenWhen: (previous, current) =>
+          previous != current && current.drawerOption != null,
+      listener: (context, state) {
+        if (MediaQuery.of(context).size.width >= PhotoboothBreakpoints.small) {
+          return Scaffold.of(context).openEndDrawer();
+        } else {
+          switch (state.drawerOption!) {
+            case DrawerOption.characters:
+              return _showCharacterBottomSheet(context);
+            case DrawerOption.props:
+              return _showPropsBottomSheet(context);
+            case DrawerOption.backgrounds:
+              return _showBackgroundBottomSheet(context);
+          }
+        }
+      },
+      child: Align(
+        alignment: screenSize >= PhotoboothBreakpoints.small
+            ? Alignment.centerRight
+            : Alignment.topCenter,
+        child: Flex(
+          mainAxisSize: MainAxisSize.min,
+          direction: screenSize >= PhotoboothBreakpoints.small
+              ? Axis.vertical
+              : Axis.horizontal,
+          children: [
+            ItemSelectorButton(
+              key: SelectionButtons.charactersSelectionButtonKey,
+              buttonBackground: const ColoredBox(color: Colors.red),
+              title: context.l10n.characterSelectorButton,
+              showTitle: screenSize >= PhotoboothBreakpoints.small,
+              onTap: () => context.read<DrawerSelectionBloc>().add(
+                    const DrawerSelectionOptionSelected(
+                      drawerOption: DrawerOption.characters,
+                    ),
+                  ),
+            ),
+            spacer,
+            ItemSelectorButton(
+              key: SelectionButtons.propsSelectionButtonKey,
+              buttonBackground: const ColoredBox(color: Colors.red),
+              title: context.l10n.propsSelectorButton,
+              showTitle: screenSize >= PhotoboothBreakpoints.small,
+              onTap: () => context.read<DrawerSelectionBloc>().add(
+                    const DrawerSelectionOptionSelected(
+                      drawerOption: DrawerOption.props,
+                    ),
+                  ),
+            ),
+            spacer,
+            ItemSelectorButton(
+              key: SelectionButtons.backgroundSelectorButtonKey,
+              buttonBackground: const ColoredBox(color: Colors.red),
+              title: context.l10n.backgroundSelectorButton,
+              showTitle: screenSize >= PhotoboothBreakpoints.small,
+              onTap: () => context.read<DrawerSelectionBloc>().add(
+                    const DrawerSelectionOptionSelected(
+                      drawerOption: DrawerOption.backgrounds,
+                    ),
+                  ),
+            ),
+          ],
+        ),
       ),
     );
   }
