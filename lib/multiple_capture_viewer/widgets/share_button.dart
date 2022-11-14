2b3bb1521c27a2968d4f35e5bc6fbf428bf68d7c
alestiago <dev@alestiago.com>
2022-11-14 13:59:24 +0000

    test: used platformHelper

--- lib/multiple_capture_viewer/widgets/share_button.dart
+++ lib/multiple_capture_viewer/widgets/share_button.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/multiple_capture_viewer/multiple_capture_viewer.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:platform_helper/platform_helper.dart';
 
 class ShareButton extends StatelessWidget {
-  const ShareButton({super.key});
+  const ShareButton({
+    super.key,
+    this.platformHelper,
+  });
+
+  /// Optional [PlatformHelper] instance.
+  ///
+  /// Used to decide whether to show a [ShareBottomSheet] or [ShareDialog].
+  final PlatformHelper? platformHelper;
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +23,7 @@ class ShareButton extends StatelessWidget {
       onPressed: () async {
         await showAppModal<void>(
           context: context,
+          platformHelper: platformHelper,
           portraitChild: const ShareBottomSheet(),
           landscapeChild: const ShareDialog(),
         );
--- test/multiple_capture_viewer/widgets/share_button_test.dart
+++ test/multiple_capture_viewer/widgets/share_button_test.dart
@@ -1,10 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:io_photobooth/multiple_capture_viewer/widgets/widgets.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:platform_helper/platform_helper.dart';
 
 import '../../helpers/helpers.dart';
 
+class _MockPlatformHelper extends Mock implements PlatformHelper {}
+
 void main() {
   group('ShareButton', () {
+    late PlatformHelper platformHelper;
+
+    setUp(() {
+      platformHelper = _MockPlatformHelper();
+    });
+
     test('can be instantiated', () {
       expect(ShareButton(), isA<ShareButton>());
     });
@@ -18,27 +28,73 @@ void main() {
     });
 
     group('onPressed', () {
-      testWidgets('opens ShareDialog when on landscape', (tester) async {
-        tester.setLandscapeDisplaySize();
+      testWidgets(
+        'opens ShareDialog when on desktop and landscape',
+        (tester) async {
+          when(() => platformHelper.isMobile).thenReturn(false);
+          tester.setLandscapeDisplaySize();
 
-        final subject = ShareButton();
-        await tester.pumpSubject(subject);
-        await tester.tap(find.byWidget(subject));
-        await tester.pumpAndSettle();
+          final subject = ShareButton(
+            platformHelper: platformHelper,
+          );
+          await tester.pumpSubject(subject);
+          await tester.tap(find.byWidget(subject));
+          await tester.pumpAndSettle();
 
-        expect(find.byType(ShareDialog), findsOneWidget);
-      });
+          expect(find.byType(ShareDialog), findsOneWidget);
+        },
+      );
 
-      testWidgets('opens ShareBottomSheet when on portrait', (tester) async {
-        tester.setPortraitDisplaySize();
+      testWidgets(
+        'opens ShareBottomSheet when on desktop and portrait',
+        (tester) async {
+          when(() => platformHelper.isMobile).thenReturn(false);
+          tester.setPortraitDisplaySize();
 
-        final subject = ShareButton();
-        await tester.pumpSubject(subject);
-        await tester.tap(find.byWidget(subject));
-        await tester.pumpAndSettle();
+          final subject = ShareButton(
+            platformHelper: platformHelper,
+          );
+          await tester.pumpSubject(subject);
+          await tester.tap(find.byWidget(subject));
+          await tester.pumpAndSettle();
 
-        expect(find.byType(ShareBottomSheet), findsOneWidget);
-      });
+          expect(find.byType(ShareBottomSheet), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'opens ShareBottomSheet when on mobile and landscape',
+        (tester) async {
+          when(() => platformHelper.isMobile).thenReturn(true);
+          tester.setLandscapeDisplaySize();
+
+          final subject = ShareButton(
+            platformHelper: platformHelper,
+          );
+          await tester.pumpSubject(subject);
+          await tester.tap(find.byWidget(subject));
+          await tester.pumpAndSettle();
+
+          expect(find.byType(ShareBottomSheet), findsOneWidget);
+        },
+      );
+
+      testWidgets(
+        'opens ShareBottomSheet when on mobile and portrait',
+        (tester) async {
+          when(() => platformHelper.isMobile).thenReturn(true);
+          tester.setPortraitDisplaySize();
+
+          final subject = ShareButton(
+            platformHelper: platformHelper,
+          );
+          await tester.pumpSubject(subject);
+          await tester.tap(find.byWidget(subject));
+          await tester.pumpAndSettle();
+
+          expect(find.byType(ShareBottomSheet), findsOneWidget);
+        },
+      );
     });
   });
 }
