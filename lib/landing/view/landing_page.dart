6ef384434cc82b3f47bda81bc9e1570ab2444eac
Oscar <martinm.oscar@gmail.com>
2022-11-15 10:04:21 +0100

    chore: import

--- lib/landing/view/landing_page.dart
+++ lib/landing/view/landing_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:io_photobooth/footer/widgets/full_footer.dart';
+import 'package:io_photobooth/footer/footer.dart';
 import 'package:io_photobooth/landing/landing.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
