4a543bbd3d12fcf60189e73971aadb83aeaf9e7c
Oscar <martinm.oscar@gmail.com>
2022-11-16 16:42:55 +0100

    feat: footer on landing and share page (#195)
    * feat: full footer
    
    * feat: footer in landing
    
    * chore: footer in share page
    
    * chore: import
    
    Co-authored-by: Alejandro Santiago <dev@alestiago.com>

--- lib/external_links/external_links.dart
+++ lib/external_links/external_links.dart
@@ -1,14 +1,6 @@
-import 'package:photobooth_ui/photobooth_ui.dart';
-
-const googleIOExternalLink = 'https://events.google.com/io/';
 const flutterDevExternalLink = 'https://flutter.dev';
 const firebaseExternalLink = 'https://firebase.google.com';
-const photoboothEmail = 'mailto:flutter-photo-booth@google.com';
-const openSourceLink = 'https://github.com/flutter/photobooth';
 const tensorFlowLink = 'https://tensorflow.org';
-
-Future<void> launchGoogleIOLink() => openLink(googleIOExternalLink);
-Future<void> launchFlutterDevLink() => openLink(flutterDevExternalLink);
-Future<void> launchFirebaseLink() => openLink(firebaseExternalLink);
-Future<void> launchPhotoboothEmail() => openLink(photoboothEmail);
-Future<void> launchOpenSourceLink() => openLink(openSourceLink);
+const mediaPipeLink = 'https://mediapipe.dev/';
+const termsOfServiceLink = 'https://policies.google.com/terms';
+const privacyPolicyLink = 'https://policies.google.com/privacy';
