8ecfa1fb14e7f74e078dddc18f82324a15e4738a
Oscar <martinm.oscar@gmail.com>
2022-11-18 17:40:30 +0100

    chore: import

--- lib/drawer_selection/widgets/drawer_layer.dart
+++ lib/drawer_selection/widgets/drawer_layer.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:io_photobooth/drawer_selection/bloc/drawer_selection_bloc.dart';
-import 'package:io_photobooth/drawer_selection/drawer_option/drawer_option.dart';
-import 'package:io_photobooth/props/bloc/props_bloc.dart';
+import 'package:io_photobooth/drawer_selection/drawer_selection.dart';
+import 'package:io_photobooth/props/props.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
 class DrawerLayer extends StatelessWidget {
