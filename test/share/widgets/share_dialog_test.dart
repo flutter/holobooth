a1772fab30cfd2a995ceb1f5a131f1800b01018e
Laura177 <laura.c.buffington@gmail.com>
2022-11-22 08:30:38 -0800

    test ShareDialog

--- test/share/widgets/share_dialog_test.dart
+++ test/share/widgets/share_dialog_test.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:io_photobooth/photo_booth/photo_booth.dart';
 import 'package:io_photobooth/share/share.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+
+import '../../helpers/helpers.dart';
+
+class FakePhotoboothCameraImage extends Fake implements PhotoboothCameraImage {
+  @override
+  PhotoConstraint get constraint => PhotoConstraint();
+  @override
+  final String data = '';
+}
 
 void main() {
   group('ShareDialog', () {
+    final image = FakePhotoboothCameraImage();
     test('can be instantiated', () {
-      expect(ShareDialog(), isA<ShareDialog>());
+      expect(
+        ShareDialog(
+          image: image,
+        ),
+        isA<ShareDialog>(),
+      );
     });
 
     group('renders', () {
+      final image = FakePhotoboothCameraImage();
       testWidgets('successfully', (tester) async {
-        final subject = ShareDialog();
+        // tester.setDisplaySize(Size(PhotoboothBreakpoints.large, 800));
+        final subject = ShareDialog(
+          image: image,
+        );
         await tester.pumpSubject(subject);
         expect(find.byWidget(subject), findsOneWidget);
       });
