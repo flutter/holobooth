83147bf0d92c691bc07cebcf98c1d6e00eeab523
Oscar <martinm.oscar@gmail.com>
2022-11-22 10:59:54 +0100

    chore: fix

--- lib/assets/assets.gen.dart
+++ lib/assets/assets.gen.dart
@@ -3,7 +3,9 @@
 ///  FlutterGen
 /// *****************************************************
 
-// ignore_for_file: directives_ordering,unnecessary_import
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal
 
 import 'package:flutter/widgets.dart';
 import 'package:rive/rive.dart';
@@ -17,6 +19,9 @@ class $AssetsAnimationsGen {
 
   /// File path: assets/animations/dash.riv
   RiveGenImage get dash => const RiveGenImage('assets/animations/dash.riv');
+
+  /// List of all assets
+  List<RiveGenImage> get values => [background, dash];
 }
 
 class $AssetsAudioGen {
@@ -24,6 +29,9 @@ class $AssetsAudioGen {
 
   /// File path: assets/audio/camera.mp3
   String get camera => 'assets/audio/camera.mp3';
+
+  /// List of all assets
+  List<String> get values => [camera];
 }
 
 class $AssetsBackgroundsGen {
@@ -48,6 +56,10 @@ class $AssetsBackgroundsGen {
   /// File path: assets/backgrounds/yellow_plus.png
   AssetGenImage get yellowPlus =>
       const AssetGenImage('assets/backgrounds/yellow_plus.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values =>
+      [blueCircle, landingBackground, photoboothBackground, redBox, yellowPlus];
 }
 
 class $AssetsCharactersGen {
@@ -59,6 +71,9 @@ class $AssetsCharactersGen {
   /// File path: assets/characters/sparky.png
   AssetGenImage get sparky =>
       const AssetGenImage('assets/characters/sparky.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [dash, sparky];
 }
 
 class $AssetsIconsGen {
@@ -87,6 +102,39 @@ class $AssetsIconsGen {
   /// File path: assets/icons/tensorflow_icon.png
   AssetGenImage get tensorflowIcon =>
       const AssetGenImage('assets/icons/tensorflow_icon.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        cameraButtonIcon,
+        firebaseIcon,
+        flutterIcon,
+        goNextButtonIcon,
+        retakeButtonIcon,
+        tensorflowIcon
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/photo_frame_spritesheet_landscape.jpg
+  AssetGenImage get photoFrameSpritesheetLandscape => const AssetGenImage(
+      'assets/images/photo_frame_spritesheet_landscape.jpg');
+
+  /// File path: assets/images/photo_frame_spritesheet_portrait.png
+  AssetGenImage get photoFrameSpritesheetPortrait =>
+      const AssetGenImage('assets/images/photo_frame_spritesheet_portrait.png');
+
+  /// File path: assets/images/photo_indicator_spritesheet.png
+  AssetGenImage get photoIndicatorSpritesheet =>
+      const AssetGenImage('assets/images/photo_indicator_spritesheet.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        photoFrameSpritesheetLandscape,
+        photoFrameSpritesheetPortrait,
+        photoIndicatorSpritesheet
+      ];
 }
 
 class Assets {
@@ -97,21 +145,26 @@ class Assets {
   static const $AssetsBackgroundsGen backgrounds = $AssetsBackgroundsGen();
   static const $AssetsCharactersGen characters = $AssetsCharactersGen();
   static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
 }
 
-class AssetGenImage extends AssetImage {
-  const AssetGenImage(String assetName) : super(assetName);
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
 
   Image image({
     Key? key,
+    AssetBundle? bundle,
     ImageFrameBuilder? frameBuilder,
-    ImageLoadingBuilder? loadingBuilder,
     ImageErrorWidgetBuilder? errorBuilder,
     String? semanticLabel,
     bool excludeFromSemantics = false,
+    double? scale,
     double? width,
     double? height,
     Color? color,
+    Animation<double>? opacity,
     BlendMode? colorBlendMode,
     BoxFit? fit,
     AlignmentGeometry alignment = Alignment.center,
@@ -120,19 +173,24 @@ class AssetGenImage extends AssetImage {
     bool matchTextDirection = false,
     bool gaplessPlayback = false,
     bool isAntiAlias = false,
+    String? package,
     FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
   }) {
-    return Image(
+    return Image.asset(
+      _assetName,
       key: key,
-      image: this,
+      bundle: bundle,
       frameBuilder: frameBuilder,
-      loadingBuilder: loadingBuilder,
       errorBuilder: errorBuilder,
       semanticLabel: semanticLabel,
       excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
       width: width,
       height: height,
       color: color,
+      opacity: opacity,
       colorBlendMode: colorBlendMode,
       fit: fit,
       alignment: alignment,
@@ -141,11 +199,18 @@ class AssetGenImage extends AssetImage {
       matchTextDirection: matchTextDirection,
       gaplessPlayback: gaplessPlayback,
       isAntiAlias: isAntiAlias,
+      package: package,
       filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
     );
   }
 
-  String get path => assetName;
+  ImageProvider provider() => AssetImage(_assetName);
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
 
 class RiveGenImage {
@@ -179,4 +244,6 @@ class RiveGenImage {
   }
 
   String get path => _assetName;
+
+  String get keyName => _assetName;
 }
