8de0268fac90be7a142ba0a9e77f73d5c7d77de4
Oscar <martinm.oscar@gmail.com>
2022-11-24 12:44:39 +0100

    chore: localizations delegates

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,6 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:avatar_detector_repository/avatar_detector_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/landing/landing.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
@@ -49,10 +48,7 @@ class _App extends StatelessWidget {
     return MaterialApp(
       title: 'I/O Photo Booth',
       theme: theme,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: const LandingPage(),
     );
