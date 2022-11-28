26ef149262247c7b8bc6d31aba02bb8df67a9fe4
alestiago <dev@alestiago.com>
2022-11-28 14:41:49 +0000

    feat: included normalizedDistance

--- lib/photo_booth/widgets/photobooth_character.dart
+++ lib/photo_booth/widgets/photobooth_character.dart
@@ -16,15 +16,35 @@ class PhotoboothCharacter extends StatelessWidget {
     final propSelected = context
         .select((InExperienceSelectionBloc bloc) => bloc.state.selectedProps);
 
-    // TODO(alestiago): Instead of directly using avatar.distance consider
-    // adding logic to define a maximum, minimum and a normal distance.
+    // TODO(alestiago): Check out if normalizations is sufficient for all
+    // characters and devices.
+    final normalizedDistance = avatar.distance.normalize(
+      fromMin: 0,
+      fromMax: 1,
+      toMin: 0.5,
+      toMax: 1,
+    );
+
     return AnimatedScale(
-      scale: avatar.distance,
+      scale: normalizedDistance,
       duration: const Duration(milliseconds: 200),
       child: DashAnimation(
         avatar: avatar,
         propsSelected: propSelected,
       ),
     );
+  }
+}
+
+extension NormalizeNumber on num {
+  double normalize({
+    required num fromMin,
+    required num fromMax,
+    required num toMin,
+    required num toMax,
+  }) {
+    assert(fromMin < fromMax, 'fromMin must be smaller than fromMax');
+    assert(toMin < toMax, 'toMin must be smaller than toMax');
+    return (toMax - toMin) * ((this - fromMin) / (fromMax - fromMin)) + toMin;
   }
 }
