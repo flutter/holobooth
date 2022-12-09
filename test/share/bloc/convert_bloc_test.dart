6708c0d52d4bb0a9ede8eb3d2b6e23174923c6e9
arturplaczek <arturp1989@gmail.com>
2022-12-09 11:47:09 +0100

    fix: add test group

--- test/share/bloc/convert_bloc_test.dart
+++ test/share/bloc/convert_bloc_test.dart
@@ -13,40 +13,42 @@ void main() {
   late ConvertRepository convertRepository;
 
   group('ConvertBloc', () {
-    blocTest<ConvertBloc, ConvertState>(
-      'return video path for request',
-      setUp: () {
-        convertRepository = _MockConvertRepository();
-        when(() => convertRepository.convertFrames(any()))
-            .thenAnswer((_) async => 'test-video-path');
-      },
-      build: () => ConvertBloc(convertRepository: convertRepository),
-      act: (bloc) => bloc.add(
-        ConvertFrames(
-          [
-            RawFrame(0, ByteData(0)),
-          ],
+    group('ConvertFrames', () {
+      blocTest<ConvertBloc, ConvertState>(
+        'return video path for request',
+        setUp: () {
+          convertRepository = _MockConvertRepository();
+          when(() => convertRepository.convertFrames(any()))
+              .thenAnswer((_) async => 'test-video-path');
+        },
+        build: () => ConvertBloc(convertRepository: convertRepository),
+        act: (bloc) => bloc.add(
+          ConvertFrames(
+            [
+              RawFrame(0, ByteData(0)),
+            ],
+          ),
         ),
-      ),
-      expect: () => [
-        ConvertLoading(),
-        ConvertSuccess('test-video-path'),
-      ],
-    );
+        expect: () => [
+          ConvertLoading(),
+          ConvertSuccess('test-video-path'),
+        ],
+      );
 
-    blocTest<ConvertBloc, ConvertState>(
-      'return ConvertError state on error',
-      setUp: () {
-        convertRepository = _MockConvertRepository();
-        when(() => convertRepository.convertFrames(any()))
-            .thenThrow(Exception());
-      },
-      build: () => ConvertBloc(convertRepository: convertRepository),
-      act: (bloc) => bloc.add(ConvertFrames(const [])),
-      expect: () => [
-        ConvertLoading(),
-        ConvertError(),
-      ],
-    );
+      blocTest<ConvertBloc, ConvertState>(
+        'return ConvertError state on error',
+        setUp: () {
+          convertRepository = _MockConvertRepository();
+          when(() => convertRepository.convertFrames(any()))
+              .thenThrow(Exception());
+        },
+        build: () => ConvertBloc(convertRepository: convertRepository),
+        act: (bloc) => bloc.add(ConvertFrames(const [])),
+        expect: () => [
+          ConvertLoading(),
+          ConvertError(),
+        ],
+      );
+    });
   });
 }
