3fe80444ebc39b96363920d9acff5872fa934050
Oscar <martinm.oscar@gmail.com>
2022-12-09 17:57:18 +0100

    chore: coverage

---
+++ test/in_experience_selection/bloc/in_experience_selection_event_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:io_photobooth/in_experience_selection/in_experience_selection.dart';
+
+void main() {
+  group('InExperienceSelectionEvent', () {
+    group('InExperienceSelectionHatSelected', () {
+      test('support value comparison', () {
+        final eventA = InExperienceSelectionHatSelected(Hats.helmet);
+        final eventB = InExperienceSelectionHatSelected(Hats.helmet);
+        final eventC = InExperienceSelectionHatSelected(Hats.none);
+        expect(eventA, equals(eventB));
+        expect(eventA, isNot(equals(eventC)));
+      });
+    });
+
+    group('InExperienceSelectionBackgroundSelected', () {
+      test('support value comparison', () {
+        final eventA =
+            InExperienceSelectionBackgroundSelected(Background.beach);
+        final eventB =
+            InExperienceSelectionBackgroundSelected(Background.beach);
+        final eventC =
+            InExperienceSelectionBackgroundSelected(Background.space);
+        expect(eventA, equals(eventB));
+        expect(eventA, isNot(equals(eventC)));
+      });
+    });
+  });
+}
