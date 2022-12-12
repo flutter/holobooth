c2f283233bf2720520769eb8071b2aef71eac38f
alestiago <dev@alestiago.com>
2022-12-12 10:16:50 +0000

    feat: used distance as a ratio

--- packages/face_geometry/lib/src/models/mouth_geometry.dart
+++ packages/face_geometry/lib/src/models/mouth_geometry.dart
@@ -3,6 +3,29 @@ import 'package:face_geometry/face_geometry.dart';
 import 'package:tensorflow_models_platform_interface/tensorflow_models_platform_interface.dart'
     as tf;
 
+class _MouthKeypoints {
+  _MouthKeypoints({
+    required this.topLip,
+    required this.bottomLip,
+  }) : distance = topLip.distanceTo(bottomLip);
+
+  /// The top lip keypoint.
+  ///
+  /// The highest lip keypoint when facing straight to the camera.
+  final tf.Keypoint topLip;
+
+  /// The bottom lip keypoint.
+  ///
+  /// The lowest lip keypoint when facing straight to the camera.
+  final tf.Keypoint bottomLip;
+
+  /// The distance between the top and bottom lip.
+  ///
+  /// Unlike [MouthGeometry.distance] this value is not a ratio but the actual
+  /// distance between the [topLip] and [bottomLip].
+  final double distance;
+}
+
 /// {@template mouth_geometry}
 /// A class that represents the geometry of the mouth.
 /// {@endtemplate}
@@ -12,65 +35,64 @@ class MouthGeometry extends Equatable {
     required List<tf.Keypoint> keypoints,
     required tf.BoundingBox boundingBox,
   }) {
-    return keypoints.length > 15
-        ? MouthGeometry._compute(keypoints: keypoints, boundingBox: boundingBox)
-        : const MouthGeometry._empty();
+    final hasEnoughKeypoints = keypoints.length > 15;
+    if (!hasEnoughKeypoints) return const MouthGeometry._empty();
+    final mouthKeypoints = _MouthKeypoints(
+      topLip: keypoints[13],
+      bottomLip: keypoints[14],
+    );
+    final canComputeRatio =
+        boundingBox.height > 0 && boundingBox.height > mouthKeypoints.distance;
+    if (!canComputeRatio) return const MouthGeometry._empty();
+
+    final distance = mouthKeypoints.distance / boundingBox.height;
+    return MouthGeometry._(
+      isOpen: _isOpen(mouthKeypoints: mouthKeypoints, distance: distance),
+      distance: distance,
+    );
   }
 
   /// {@macro mouth_geometry}
-  MouthGeometry._compute({
-    required List<tf.Keypoint> keypoints,
-    required tf.BoundingBox boundingBox,
-  })  : isOpen = _isOpen(
-          topLip: keypoints[13],
-          bottomLip: keypoints[14],
-          faceHeight: boundingBox.height,
-        ),
-        distance = _distance(
-          topLip: keypoints[13],
-          bottomLip: keypoints[14],
-        );
+  const MouthGeometry._({
+    required this.isOpen,
+    required this.distance,
+  });
 
   /// An empty instance of [MouthGeometry].
   ///
-  /// This is used when the keypoints are not available.
+  /// This is the default value of [MouthGeometry] whenever there is not enough
+  /// data to compute the geometry. Like for example, when there are not enough
+  /// keypoints or the bounding box is too small.
   const MouthGeometry._empty()
       : isOpen = false,
         distance = 0;
 
-  /// This represents the minimum height of the mouth in comparison to the face
-  /// height.
+  /// Eye-balled distance that represents the minimum height of the mouth to be
+  /// considered open.
   static const _minMouthFaceRatio = 0.02;
 
-  static double _distance({
-    required tf.Keypoint topLip,
-    required tf.Keypoint bottomLip,
-  }) {
-    return topLip.distanceTo(bottomLip);
-  }
-
   static bool _isOpen({
-    required tf.Keypoint topLip,
-    required tf.Keypoint bottomLip,
-    required num faceHeight,
+    required _MouthKeypoints mouthKeypoints,
+    required num distance,
   }) {
-    final distance = topLip.distanceTo(bottomLip);
-    if (distance > 1) {
-      final heightRatio = distance / faceHeight;
-      if (heightRatio == 0) {
+    if (mouthKeypoints.distance > 1) {
+      if (distance == 0) {
         return false;
       } else {
-        return heightRatio > _minMouthFaceRatio;
+        return distance > _minMouthFaceRatio;
       }
     }
-
     return false;
   }
 
-  /// Defines if the mouth is open based on hight and face height.
+  /// Defines if the mouth is open.
   final bool isOpen;
 
   /// The distance between the top and bottom lip.
+  ///
+  /// To avoid uncorrelated distances due to how close the face is to the camera
+  /// the distance is given as a ratio of the face height. Therefore the value
+  /// is between 0 and 1.
   final double distance;
 
   @override
