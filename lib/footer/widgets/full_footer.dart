54e848b0b481d3c2721bdb38406e5ef48767637c
Will Hlas <75334479+willhlas@users.noreply.github.com>
2022-12-13 12:17:51 -0600

    fix: footer constraints

--- lib/footer/widgets/full_footer.dart
+++ lib/footer/widgets/full_footer.dart
@@ -9,66 +9,69 @@ class FullFooter extends StatelessWidget {
   Widget build(BuildContext context) {
     const gap = SizedBox(width: 32);
 
-    return ResponsiveLayoutBuilder(
-      small: (context, child) {
-        return Container(
-          alignment: Alignment.bottomCenter,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 8,
-          ).copyWith(
-            bottom: 8,
-          ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 12,
-          ),
-          decoration: BoxDecoration(
-            color: const Color(0xFF1D1D1D).withOpacity(0.5),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: child,
-        );
-      },
-      large: (context, child) {
-        return Container(
-          alignment: Alignment.bottomCenter,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 12,
-          ),
-          child: Row(
-            children: [
-              const FlutterIconLink(),
-              gap,
-              const FirebaseIconLink(),
-              gap,
-              const TensorflowIconLink(),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: child,
-                ),
-              )
-            ],
-          ),
-        );
-      },
-      child: Wrap(
-        alignment: WrapAlignment.center,
-        runSpacing: 8,
-        children: const [
-          FlutterFooterLink(),
-          gap,
-          FirebaseFooterLink(),
-          gap,
-          TensorFlowFooterLink(),
-          gap,
-          MediapipeFooterLink(),
-          gap,
-          FooterTermsOfServiceLink(),
-          gap,
-          FooterPrivacyPolicyLink(),
-        ],
+    return SizedBox(
+      height: 100,
+      child: ResponsiveLayoutBuilder(
+        small: (context, child) {
+          return Container(
+            alignment: Alignment.bottomCenter,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 8,
+            ).copyWith(bottom: 8),
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 12,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1D1D1D).withOpacity(0.5),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: child,
+            ),
+          );
+        },
+        large: (context, child) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 24,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                const FlutterIconLink(),
+                gap,
+                const FirebaseIconLink(),
+                gap,
+                const TensorflowIconLink(),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: child,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          runSpacing: 8,
+          children: const [
+            FlutterFooterLink(),
+            gap,
+            FirebaseFooterLink(),
+            gap,
+            TensorFlowFooterLink(),
+            gap,
+            MediapipeFooterLink(),
+            gap,
+            FooterTermsOfServiceLink(),
+            gap,
+            FooterPrivacyPolicyLink(),
+          ],
+        ),
       ),
     );
   }
