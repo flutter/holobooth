5f926993df9e61c31d4f913c028e61a7f2aeaf8d
Oscar <martinm.oscar@gmail.com>
2022-12-16 11:57:38 +0100

    chore: show basic dialog

--- lib/share/widgets/share_button.dart
+++ lib/share/widgets/share_button.dart
@@ -4,19 +4,12 @@ import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/photo_booth/photo_booth.dart';
 import 'package:io_photobooth/share/share.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
-import 'package:platform_helper/platform_helper.dart';
 
 class ShareButton extends StatelessWidget {
   const ShareButton({
     super.key,
-    this.platformHelper,
     required this.image,
   });
-
-  /// Optional [PlatformHelper] instance.
-  ///
-  /// Used to decide whether to show a [ShareBottomSheet] or [ShareDialog].
-  final PlatformHelper? platformHelper;
 
   final PhotoboothCameraImage image;
 
@@ -28,19 +21,12 @@ class ShareButton extends StatelessWidget {
       style: ElevatedButton.styleFrom(
         backgroundColor: PhotoboothColors.white,
       ),
-      onPressed: () async {
-        await showAppModal<void>(
+      onPressed: () {
+        showAppDialog<void>(
           context: context,
-          platformHelper: platformHelper,
-          portraitChild: BlocProvider.value(
+          child: BlocProvider.value(
             value: context.read<ShareBloc>(),
-            child: ShareBottomSheet(image: image),
-          ),
-          landscapeChild: BlocProvider.value(
-            value: context.read<ShareBloc>(),
-            child: ShareDialog(
-              image: image,
-            ),
+            child: const ShareDialog(),
           ),
         );
       },
--- lib/share/widgets/share_dialog.dart
+++ lib/share/widgets/share_dialog.dart
@@ -1,90 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
-import 'package:io_photobooth/photo_booth/photo_booth.dart';
-import 'package:io_photobooth/share/widgets/facebook_button.dart';
-import 'package:io_photobooth/share/widgets/share_preview_photo.dart';
-import 'package:io_photobooth/share/widgets/share_social_media_clarification.dart';
-import 'package:io_photobooth/share/widgets/twitter_button.dart';
-import 'package:photobooth_ui/photobooth_ui.dart';
 
 class ShareDialog extends StatelessWidget {
-  const ShareDialog({super.key, required this.image});
-
-  final PhotoboothCameraImage image;
+  const ShareDialog({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final l10n = context.l10n;
-    return DecoratedBox(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            PhotoboothColors.whiteBackground,
-            PhotoboothColors.white,
+    return Container(
+      margin: const EdgeInsets.all(150),
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(38),
+        gradient: const LinearGradient(
+          colors: <Color>[
+            Color(0xFF9E81EF),
+            Color(0xFF4100E0),
           ],
         ),
       ),
-      child: Stack(
-        children: [
-          SingleChildScrollView(
-            child: SizedBox(
-              width: 900,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 30,
-                  vertical: 60,
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    SharePreviewPhoto(
-                      image: image,
-                    ),
-                    const SizedBox(height: 60),
-                    SelectableText(
-                      l10n.shareDialogHeading,
-                      key: const Key('shareDialog_heading'),
-                      style: theme.textTheme.displayLarge,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 24),
-                    SelectableText(
-                      l10n.shareDialogSubheading,
-                      key: const Key('shareDialog_subheading'),
-                      style: theme.textTheme.displaySmall,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 30),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        TwitterButton(),
-                        SizedBox(width: 36),
-                        FacebookButton(),
-                      ],
-                    ),
-                    const SizedBox(height: 30),
-                    const SocialMediaShareClarificationNote(),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: 24,
-            top: 24,
-            child: IconButton(
-              icon: const Icon(
-                Icons.clear,
-                color: PhotoboothColors.black54,
-              ),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-          ),
-        ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF020320).withOpacity(0.95),
+          borderRadius: BorderRadius.circular(38),
+        ),
       ),
     );
   }
