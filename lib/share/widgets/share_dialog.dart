137fa406ecf2f62d149ce81a297551ab9bb98355
Oscar <martinm.oscar@gmail.com>
2022-12-16 16:01:37 +0100

    chore: fix sizing

--- lib/share/widgets/share_dialog.dart
+++ lib/share/widgets/share_dialog.dart
@@ -1,32 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:io_photobooth/share/widgets/widgets.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
 
 class ShareDialog extends StatelessWidget {
   const ShareDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(1),
-      constraints: const BoxConstraints(
-        maxHeight: 2000,
-        minHeight: 1000,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(38),
-        gradient: const LinearGradient(
-          colors: <Color>[
-            Color(0xFF9E81EF),
-            Color(0xFF4100E0),
-          ],
-        ),
-      ),
-      child: DecoratedBox(
+    return AlertDialog(
+      contentPadding: EdgeInsets.zero,
+      backgroundColor: PhotoboothColors.transparent,
+      content: Container(
+        padding: const EdgeInsets.all(1),
+        height: 600,
+        width: 900,
         decoration: BoxDecoration(
-          color: const Color(0xFF020320).withOpacity(0.95),
           borderRadius: BorderRadius.circular(38),
+          gradient: const LinearGradient(
+            colors: <Color>[
+              Color(0xFF9E81EF),
+              Color(0xFF4100E0),
+            ],
+          ),
         ),
-        child: const ShareDialogBody(),
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: const Color(0xFF020320).withOpacity(0.95),
+            borderRadius: BorderRadius.circular(38),
+          ),
+          child: const ShareDialogBody(),
+        ),
       ),
     );
   }
