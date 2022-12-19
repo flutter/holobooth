5ae9343b5a3905ebac1080f0fce500d006c8293b
Will Hlas <75334479+willhlas@users.noreply.github.com>
2022-12-19 12:51:24 -0600

    feat: update share page design (#261)
    * feat: update share page design
    
    * chore: small
    
    * chore: fix
    
    * chore: format
    
    Co-authored-by: Oscar <martinm.oscar@gmail.com>

---
+++ lib/share/widgets/download_button.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:io_photobooth/l10n/l10n.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+
+class DownloadButton extends StatelessWidget {
+  const DownloadButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return GradientOutlinedButton(
+      icon: const Icon(Icons.file_download_rounded),
+      label: Text(l10n.sharePageDownloadButtonText),
+    );
+  }
+}
