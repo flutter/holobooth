ae628f054aff8d2553fec1007911e171eafed9e3
Oscar <martinm.oscar@gmail.com>
2023-01-04 19:36:57 +0100

    chore: style

--- lib/convert/view/convert_page.dart
+++ lib/convert/view/convert_page.dart
@@ -86,7 +86,6 @@ class ConvertBody extends StatelessWidget {
               builder: (context, state) {
                 switch (state.status) {
                   case ConvertStatus.loadingFrames:
-                    return ProgressIndicatorExample();
                     return GradientText(
                       text: 'Frames processed ${state.framesProcessed}...',
                       style: PhotoboothTextStyle.displayMedium,
@@ -121,63 +120,6 @@ class ConvertLoadingBody extends StatelessWidget {
         SizedBox(height: 50),
         ConvertMessage(),
       ],
-    );
-  }
-}
-
-class ProgressIndicatorExample extends StatefulWidget {
-  const ProgressIndicatorExample({super.key});
-
-  @override
-  State<ProgressIndicatorExample> createState() =>
-      _ProgressIndicatorExampleState();
-}
-
-class _ProgressIndicatorExampleState extends State<ProgressIndicatorExample>
-    with TickerProviderStateMixin {
-  late AnimationController controller;
-
-  @override
-  void initState() {
-    controller = AnimationController(
-      /// [AnimationController]s can be created with `vsync: this` because of
-      /// [TickerProviderStateMixin].
-      vsync: this,
-      duration: const Duration(seconds: 5),
-    )..addListener(() {
-        setState(() {});
-      });
-    controller.repeat(reverse: true);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          const Text(
-            'Linear progress indicator with a fixed color',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(
-            width: 100,
-            child: LinearProgressIndicator(
-              value: controller.value,
-              semanticsLabel: 'Linear progress indicator',
-              minHeight: 50,
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
