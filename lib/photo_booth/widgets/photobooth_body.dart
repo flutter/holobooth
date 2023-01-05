22d64d7b7db3d0616191fa562dcc477968523a24
Kirpal Demian <kirpal@verygood.ventures>
2023-01-05 12:10:41 -0500

    fix: record just the character and background

--- lib/photo_booth/widgets/photobooth_body.dart
+++ lib/photo_booth/widgets/photobooth_body.dart
@@ -67,47 +67,52 @@ class _PhotoboothBodyState extends State<PhotoboothBody> {
 
     return LayoutBuilder(
       builder: (context, contrains) {
-        return ScreenRecorder(
-          width: contrains.maxWidth,
-          height: contrains.maxHeight,
-          controller: _screenRecorderController,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              const PhotoboothBackground(),
-              const Align(
-                alignment: Alignment.bottomCenter,
-                child: SimplifiedFooter(),
+        return Stack(
+          fit: StackFit.expand,
+          children: [
+            ScreenRecorder(
+              width: contrains.maxWidth,
+              height: contrains.maxHeight,
+              controller: _screenRecorderController,
+              child: Stack(
+                fit: StackFit.expand,
+                children: const [
+                  PhotoboothBackground(),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: PhotoboothCharacter(),
+                  ),
+                ],
               ),
-              const Align(
-                alignment: Alignment.bottomCenter,
-                child: PhotoboothCharacter(),
-              ),
-              Align(child: CameraView(onCameraReady: _onCameraReady)),
-              if (_isCameraAvailable)
-                CameraStreamListener(cameraController: _cameraController!),
-              if (_isCameraAvailable &&
-                  avatarStatus == AvatarDetectorStatus.notDetected)
-                const Align(child: HoloBoothCharacterError()),
-              BlocBuilder<PhotoBoothBloc, PhotoBoothState>(
-                builder: (_, state) {
-                  if (state.isRecording) {
-                    return Align(
-                      alignment: Alignment.bottomCenter,
-                      child: RecordingCountdown(
-                        onCountdownCompleted: _stopRecording,
-                      ),
-                    );
-                  } else if (state.gettingReady) {
-                    return GetReadyLayer(
-                      onCountdownCompleted: _startRecording,
-                    );
-                  }
-                  return const SelectionLayer();
-                },
-              ),
-            ],
-          ),
+            ),
+            const Align(
+              alignment: Alignment.bottomCenter,
+              child: SimplifiedFooter(),
+            ),
+            Align(child: CameraView(onCameraReady: _onCameraReady)),
+            if (_isCameraAvailable)
+              CameraStreamListener(cameraController: _cameraController!),
+            if (_isCameraAvailable &&
+                avatarStatus == AvatarDetectorStatus.notDetected)
+              const Align(child: HoloBoothCharacterError()),
+            BlocBuilder<PhotoBoothBloc, PhotoBoothState>(
+              builder: (_, state) {
+                if (state.isRecording) {
+                  return Align(
+                    alignment: Alignment.bottomCenter,
+                    child: RecordingCountdown(
+                      onCountdownCompleted: _stopRecording,
+                    ),
+                  );
+                } else if (state.gettingReady) {
+                  return GetReadyLayer(
+                    onCountdownCompleted: _startRecording,
+                  );
+                }
+                return const SelectionLayer();
+              },
+            ),
+          ],
         );
       },
     );
