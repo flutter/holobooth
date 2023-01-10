b67dd8e2fd4e844c835eabe12d2b10d017f7cf6c
Erick Zanardo <erickzanardoo@gmail.com>
2023-01-10 17:46:24 -0300

    fixing domains

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -9,4 +9,5 @@ export const ALLOWED_HOSTS = [
   'us-central1-io-photo-booth.cloudfunctions.net',
   'photobooth.flutter.dev',
   'holobooth-prod.web.app',
+  '0cf34931-6ab3-8cd9-57b5-f38b5fa5ba1e.web.app',
 ];
