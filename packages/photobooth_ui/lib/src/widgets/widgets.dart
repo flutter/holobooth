15e06d845691d55428db38885b100ca3e5c7731e
Oscar <martinm.oscar@gmail.com>
2023-01-11 13:12:54 +0100

    chore: delete animated pulse

--- packages/photobooth_ui/lib/src/widgets/widgets.dart
+++ packages/photobooth_ui/lib/src/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'animated_fade_in.dart';
-export 'animated_pulse.dart';
 export 'animated_sprite.dart';
 export 'app_animated_cross_fade.dart';
 export 'app_circular_progress_indicator.dart';
