531c2079e4494cf74ac071ce0563d67c046ce734
Oscar <martinm.oscar@gmail.com>
2023-01-13 10:36:36 +0100

    Update lib/main_prod.dart
    Co-authored-by: Erick <erickzanardoo@gmail.com>

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -4,7 +4,7 @@ import 'package:io_photobooth/firebase_options_prod.dart';
 Future<void> main() async {
   const convertUrl = 'https://convert-fge4q4vwia-uc.a.run.app';
   const shareUrl =
-      'https://96e0700d-fe2c-4609-b43c-87093e447b75.web.app/share/';
+      'https://holobooth.flutter.dev/share/';
   const assetBucketUrl =
       'https://storage.googleapis.com/io-photobooth-dev.appspot.com/uploads/';
   await bootstrap(
