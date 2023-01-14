f5a2acb05933619b1f0a7feac4d36707506445b8
Oscar <martinm.oscar@gmail.com>
2023-01-14 14:16:50 +0100

    chore: format

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,8 +3,7 @@ import 'package:io_photobooth/firebase_options_prod.dart';
 
 Future<void> main() async {
   const convertUrl = 'https://convert-fge4q4vwia-uc.a.run.app';
-  const shareUrl =
-      'https://holobooth.flutter.dev/share/';
+  const shareUrl = 'https://holobooth.flutter.dev/share/';
   const assetBucketUrl =
       'https://storage.googleapis.com/holobooth-prod.appspot.com/uploads/';
   await bootstrap(
