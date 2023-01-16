cb34543a1ca30ee6b224fd76331c5e123cbca87d
Erick Zanardo <erickzanardoo@gmail.com>
2023-01-16 15:56:01 -0300

    fix tests

--- test/share/widgets/download_button_test.dart
+++ test/share/widgets/download_button_test.dart
@@ -1,12 +1,24 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:holobooth/convert/convert.dart';
 import 'package:holobooth/share/share.dart';
 
 import '../../helpers/helpers.dart';
 
+class _MockConvertBloc extends MockBloc<ConvertEvent, ConvertState>
+    implements ConvertBloc {}
+
 void main() {
   group('DownloadButton', () {
     testWidgets('opens DownloadOptionDialog on tap', (tester) async {
-      await tester.pumpApp(DownloadButton());
+      await tester.pumpApp(
+        buildSubject(
+          DownloadButton(),
+          convertBloc: _MockConvertBloc(),
+        ),
+      );
       await tester.tap(find.byType(DownloadButton));
       await tester.pumpAndSettle();
       expect(find.byType(DownloadOptionDialog), findsOneWidget);
@@ -14,7 +26,12 @@ void main() {
 
     testWidgets('closes DownloadOptionDialog tapping on DownloadAsAGifButton',
         (tester) async {
-      await tester.pumpApp(DownloadButton());
+      await tester.pumpApp(
+        buildSubject(
+          DownloadButton(),
+          convertBloc: _MockConvertBloc(),
+        ),
+      );
       await tester.tap(find.byType(DownloadButton));
       await tester.pumpAndSettle();
       await tester.tap(find.byType(DownloadAsAGifButton));
@@ -24,7 +41,12 @@ void main() {
 
     testWidgets('closes DownloadOptionDialog tapping on DownloadAsAVideoButton',
         (tester) async {
-      await tester.pumpApp(DownloadButton());
+      await tester.pumpApp(
+        buildSubject(
+          DownloadButton(),
+          convertBloc: _MockConvertBloc(),
+        ),
+      );
       await tester.tap(find.byType(DownloadButton));
       await tester.pumpAndSettle();
       await tester.tap(find.byType(DownloadAsAVideoButton));
@@ -32,4 +54,11 @@ void main() {
       expect(find.byType(DownloadOptionDialog), findsNothing);
     });
   });
+}
+
+Widget buildSubject(
+  Widget subject, {
+  required ConvertBloc convertBloc,
+}) {
+  return BlocProvider.value(value: convertBloc, child: subject);
 }
