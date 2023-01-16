d15a47dc54c287e2b5ac4ec6fbd3ce8a007270a3
Oscar <martinm.oscar@gmail.com>
2023-01-16 10:36:24 +0100

    chore: remove tensorflow gallery

--- packages/tensorflow_models/tensorflow_models/example/lib/src/pages/pages.dart
+++
@@ -1,4 +0,0 @@
-export 'landmarks_dash_page.dart';
-export 'landmarks_detect_blink_page.dart';
-export 'landmarks_open_mouth_page.dart';
-export 'landmarks_video_stream_page.dart';
--- packages/tensorflow_models/tensorflow_models/example/lib/src/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'camera_view.dart';
-export 'face_geometry_overlay.dart';
-export 'faces_detector_builder.dart';
