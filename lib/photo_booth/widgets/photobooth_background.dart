25abfc3866ee3068c39e97680fa0928f7bb83a7a
Oscar <martinm.oscar@gmail.com>
2023-01-16 18:23:42 +0100

    feat: split backgrounds (#344)
    * chore: wip
    
    * feat: split backgrounds
    
    * chore: remove
    
    * chore: nit
    
    * chore: rename
    
    * chore: import
    
    * chore: remove rive index
    
    * chore: rename
    
    * chore: run workflow

--- lib/photo_booth/widgets/photobooth_background.dart
+++ lib/photo_booth/widgets/photobooth_background.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:holobooth/assets/assets.dart';
 import 'package:holobooth/in_experience_selection/in_experience_selection.dart';
 import 'package:holobooth/rive/rive.dart';
 
@@ -10,8 +11,41 @@ class PhotoboothBackground extends StatelessWidget {
   Widget build(BuildContext context) {
     final backgroundSelected = context
         .select((InExperienceSelectionBloc bloc) => bloc.state.background);
+
+    RiveGenImage riveAsset;
+
+    switch (backgroundSelected) {
+      case Background.bg0:
+        riveAsset = Assets.animations.bg00;
+        break;
+      case Background.bg1:
+        riveAsset = Assets.animations.bg01;
+        break;
+      case Background.bg2:
+        riveAsset = Assets.animations.bg02;
+        break;
+      case Background.bg3:
+        riveAsset = Assets.animations.bg03;
+        break;
+      case Background.bg4:
+        riveAsset = Assets.animations.bg04;
+        break;
+      case Background.bg5:
+        riveAsset = Assets.animations.bg05;
+        break;
+      case Background.bg6:
+        riveAsset = Assets.animations.bg06;
+        break;
+      case Background.bg7:
+        riveAsset = Assets.animations.bg07;
+        break;
+      case Background.bg8:
+        riveAsset = Assets.animations.bg08;
+        break;
+    }
     return BackgroundAnimation(
-      backgroundSelected: backgroundSelected,
+      key: Key(backgroundSelected.name),
+      riveGenImage: riveAsset,
     );
   }
 }
