9e6a5e9ba989677ed20ab1c3761604e815ae66c3
Oscar <martinm.oscar@gmail.com>
2023-01-16 16:14:29 +0100

    chore: merge

--- lib/share/widgets/share_dialog_close_button.dart
+++ lib/share/widgets/share_dialog_close_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:holobooth/assets/assets.dart';
-import 'package:io_photobooth/assets/assets.dart';
-import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:holobooth_ui/holobooth_ui.dart';
 
 class ShareDialogCloseButton extends StatelessWidget {
   const ShareDialogCloseButton({super.key, required this.size});
