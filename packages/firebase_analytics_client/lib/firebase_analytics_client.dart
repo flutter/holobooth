1cc2560e0c2c6d90654298846cedce89b6559020
Oscar <martinm.oscar@gmail.com>
2023-01-18 18:28:38 +0100

    chore: wip

--- packages/firebase_analytics_client/lib/firebase_analytics_client.dart
+++ packages/firebase_analytics_client/lib/firebase_analytics_client.dart
@@ -1,4 +1,4 @@
-library analytics;
+library firebase_analytics_client;
 
 export 'src/firebase_analytics_client_io.dart'
     if (dart.library.html) 'src/firebase_analytics_client_web.dart';
---
+++ packages/firebase_analytics_client/test/src/firebase_analytics_client_io_test.dart
@@ -0,0 +1,20 @@
+@TestOn('!chrome')
+import 'package:firebase_analytics_client/firebase_analytics_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('FirebaseAnalyticsClient', () {
+    group('trackEvent', () {
+      test('returns normally', () {
+        expect(
+          () => FirebaseAnalyticsClient().trackEvent(
+            category: 'category',
+            action: 'action',
+            label: 'label',
+          ),
+          returnsNormally,
+        );
+      });
+    });
+  });
+}
